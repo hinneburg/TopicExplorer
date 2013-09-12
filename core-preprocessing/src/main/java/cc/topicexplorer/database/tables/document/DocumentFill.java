f138696038d710eddd0560b7be837e6f196f9c25
Alexander Hinneburg <hinneburg@informatik.uni-halle.de>
2013-09-12 11:43:40 +0000

    Nicht genutzten Import entfernt zum Testen des SVN.

--- core-preprocessing/src/main/java/cc/topicexplorer/database/tables/document/DocumentFill.java
+++ core-preprocessing/src/main/java/cc/topicexplorer/database/tables/document/DocumentFill.java
@@ -1,8 +1,6 @@
 package cc.topicexplorer.database.tables.document;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
-
 import cc.topicexplorer.chain.commands.TableFillCommand;
 /** MIT-JOOQ-START 
 import chain.commands.TableFillCommand;

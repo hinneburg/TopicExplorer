f05a21f534127d6a66e9978eeb8946e0fd22e4e7
Benjamin Schandera <4.32@gmx.de>
2013-10-22 13:46:52 +0000

    write test cases for ChainManagement.java

---
+++ command-manager/src/test/java/cc/topicexplorer/chain/ChainManagementTest.java
@@ -0,0 +1,34 @@
+package cc.topicexplorer.chain;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+import cc.topicexplorer.exceptions.CatalogNotInstantiableException;
+
+public class ChainManagementTest {
+
+	private final ChainManagement _chainManager = new ChainManagement();
+
+	@Test
+	public void testSetCatalog() {
+		try {
+			_chainManager.setCatalog("/DummyCatalog.xml");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		assertThat(_chainManager.catalog).isNotNull();
+	}
+
+	@Test(expected = CatalogNotInstantiableException.class)
+	public void testSetCatalogWithNoValidCatalog() throws Exception {
+		try {
+			_chainManager.setCatalog("");
+			fail();
+		} catch (Exception e) {
+			throw e;
+		}
+	}
+
+}

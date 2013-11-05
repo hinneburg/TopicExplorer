584ab1d148349fa03ba7694ad5f81c2a27ea4d9e
Gert Boehmer <gertb@gmx.de>
2013-11-05 11:11:59 +0000

    wiki plug-in

--- plugin-wiki-preprocessing/src/main/java/cc/topicexplorer/plugin/wiki/preprocessing/tables/orgtable/OrgTableCreateAndFill.java
+++ plugin-wiki-preprocessing/src/main/java/cc/topicexplorer/plugin/wiki/preprocessing/tables/orgtable/OrgTableCreateAndFill.java
@@ -21,67 +21,66 @@ public class OrgTableCreateAndFill extends TableCommand {
 
 	@Override
 	public void tableExecute(Context context) {
-//		String tableName = this.properties.getProperty("OrgTableName");
-//
-//		// drop table
-//
-//		try {
-//			this.database.executeUpdateQuery("drop table " + tableName + ";");
-//		} catch (SQLException e) {
-//			logger.warn("wiki-plug-in." + tableName + " : Cannot drop table "
-//					+ tableName
-//					+ ", perhaps it doesn't exists. Doesn't matter ;)");
-//		}
-//
-//		String sql = "CREATE TABLE `" + tableName + "` (" + "`"
-//				+ this.properties.getProperty("OrgTableId")
-//				+ "` bigint(11) NOT NULL, " + "`"
-//				+ this.properties.getProperty("OrgTableTxt")
-//				+ "` text COLLATE utf8_bin NOT NULL, " + "`"
-//				+ this.properties.getProperty("Text_OrgTableTitle")
-//				+ "` text COLLATE utf8_bin NOT NULL , "
-//				+ " UNIQUE KEY `id` (`id`) "
-//				+ " ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin; ";
-//
-//		// create table
-//
-//		try {
-//			this.database.executeUpdateQuery(sql);
-//		} catch (SQLException e) {
-//			logger.fatal("wiki-plug-in." + tableName
-//					+ ": cannot create table \n. Program will exit.\n" ) ;
-//			e.printStackTrace();
-//			System.exit(0);
-//		}
-//
-//		// fill table with values from the output of the wiki plug-in
-//
-//		sql = " START TRANSACTION ; "
-//				+ " LOAD DATA LOCAL INFILE "
-//				+ "'"
-//				+ properties.getProperty("Wiki_outputFolder")
-//				+ System.getProperty("file.separator") + "inputsql.csv"
-//				+ "' "
-//				+ "INTO TABLE "
-//				+ tableName
-//				+ " "
-//				+ "CHARACTER set utf8 FIELDS TERMINATED BY ';' ENCLOSED BY '\"'   "
-//				+ "LINES TERMINATED BY '|s|q|l|e|n|d|i|n|g|\n' " + " ("
-//				+ this.properties.getProperty("OrgTableId") + ", "
-//				+ this.properties.getProperty("Text_OrgTableTitle") + ","
-//				+ this.properties.getProperty("OrgTableTxt") + " ) ; "
-//				+ "COMMIT;";
-//
-//		System.out.println(sql);
-//		
-//		try {
-//			this.database.executeUpdateQuery(sql);
-//		} catch (SQLException e) {
-//			logger.fatal("wiki-plug-in."+tableName+": failure in filling the table.\n programm will exit.");
-//			//fatal
-//			e.printStackTrace();
-//			System.exit(1);
-//		}
+		String tableName = this.properties.getProperty("OrgTableName");
+
+		// drop table
+
+		try {
+			this.database.executeUpdateQuery("drop table " + tableName + ";");
+		} catch (SQLException e) {
+			logger.warn("wiki-plug-in." + tableName + " : Cannot drop table "
+					+ tableName
+					+ ", perhaps it doesn't exists. Doesn't matter ;)");
+		}
+
+		String sql = "CREATE TABLE `" + tableName + "` (" + "`"
+				+ this.properties.getProperty("OrgTableId")
+				+ "` bigint(11) NOT NULL, " + "`"
+				+ this.properties.getProperty("OrgTableTxt")
+				+ "` text COLLATE utf8_bin NOT NULL, " + "`"
+				+ this.properties.getProperty("Text_OrgTableTitle")
+				+ "` text COLLATE utf8_bin NOT NULL , "
+				+ " UNIQUE KEY `id` (`id`) "
+				+ " ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin; ";
+
+		// create table
+
+		try {
+			this.database.executeUpdateQuery(sql);
+		} catch (SQLException e) {
+			logger.fatal("wiki-plug-in." + tableName
+					+ ": cannot create table \n. Program will exit.\n" ) ;
+			e.printStackTrace();
+			System.exit(0);
+		}
+
+		// fill table with values from the output of the wiki plug-in
+
+			sql = " LOAD DATA LOCAL INFILE "
+				+ "'"
+				+ properties.getProperty("Wiki_outputFolder")
+				+ System.getProperty("file.separator") + "inputsql.csv"
+				+ "' "
+				+ "INTO TABLE "
+				+ tableName
+				+ " "
+				+ "CHARACTER set utf8 FIELDS TERMINATED BY ';' ENCLOSED BY '\"'   "
+				+ "LINES TERMINATED BY '|s|q|l|e|n|d|i|n|g|\n' " + " ("
+				+ this.properties.getProperty("OrgTableId") + ", "
+				+ this.properties.getProperty("Text_OrgTableTitle") + ","
+				+ this.properties.getProperty("OrgTableTxt") + " ) ; ";
+
+
+		System.out.println(sql);
+		
+		try {
+			this.database.executeUpdateQuery(sql);
+		} catch (SQLException e) {
+			logger.fatal("wiki-plug-in."+tableName+": failure in filling the table.\n programm will exit.");
+			//fatal
+			e.printStackTrace();
+			System.exit(1);
+		}
 		
 	}
 
--- plugin-wiki-preprocessing/src/main/java/wikiParser/Prehelper.java
+++ plugin-wiki-preprocessing/src/main/java/wikiParser/Prehelper.java
@@ -1,4 +1,5 @@
 package wikiParser;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -20,111 +21,101 @@ import java.util.concurrent.TimeUnit;
 
 import wikiParser.Supporter;
 
-public class Prehelper
-{
+public class Prehelper {
 
 	private Database db;
 	private Properties prop;
 	private Stopwatch stopWatch;
-	
+
 	private final String fileSeparator = System.getProperty("file.separator");
 
 	private final String fileNameInputSQLParsed = "inputsql.csv";
 	private final String fileNameInputMallet = "inputmallet.csv";
 	private final String fileNameLogging = "logging.txt";
-	
-//	final static Logger logger = Logger.getRootLogger();
-	
-	public Prehelper(Properties prop)
-	{
-		this.prop=prop;
-		
-		try
-		{
+
+	// final static Logger logger = Logger.getRootLogger();
+
+	public Prehelper(Properties prop) {
+		this.prop = prop;
+
+		try {
 			this.init();
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	private void init() throws Exception
-	{
+
+	private void init() throws Exception {
 		db = new Database(prop);
-		
-		String outputFolder = prop.getProperty("Wiki_outputFolder"); 
+
+		String outputFolder = prop.getProperty("Wiki_outputFolder");
 		File dir = new File(outputFolder);
 		dir.mkdir();
 
 		dir = new File(outputFolder + fileSeparator + "temp");
 		dir.mkdir();
-		
 
 		stopWatch = new Stopwatch();
 	}
-	
-	
 
-	
 	/**
-	 * reads the output-directory and joins the outputfiles into one 
+	 * reads the output-directory and joins the outputfiles into one
 	 */
-	private void joinTheOutputsAndDeleteTempFilesInTempFolder()
-	{
+	private void joinTheOutputsAndDeleteTempFilesInTempFolder() {
 
-		System.out.println("joining the outputfiles and deleting the temp files");
-		
-		try
-		{
+		System.out
+				.println("joining the outputfiles and deleting the temp files");
+
+		try {
 			String outputFolder = prop.getProperty("Wiki_outputFolder");
-			
+
 			File tempFolder = new File(outputFolder + fileSeparator + "temp");
 			File[] fileList = tempFolder.listFiles();
 
-			File fileInputMallet = new File(outputFolder + fileSeparator+ fileNameInputMallet);
-			File fileInputSQLParsed = new File(outputFolder + fileSeparator+ fileNameInputSQLParsed);
-			File fileLogging = new File(outputFolder + fileSeparator+ fileNameLogging);
-		
-			this.mergeDataIntoOneFile(fileInputMallet, fileList, "malletWikiText");
-			this.mergeDataIntoOneFile(fileInputSQLParsed, fileList, "inputParsedText");
+			File fileInputMallet = new File(outputFolder + fileSeparator
+					+ fileNameInputMallet);
+			File fileInputSQLParsed = new File(outputFolder + fileSeparator
+					+ fileNameInputSQLParsed);
+			File fileLogging = new File(outputFolder + fileSeparator
+					+ fileNameLogging);
+
+			this.mergeDataIntoOneFile(fileInputMallet, fileList,
+					"malletWikiText");
+			this.mergeDataIntoOneFile(fileInputSQLParsed, fileList,
+					"inputParsedText");
 			this.mergeDataIntoOneFile(fileLogging, fileList, "logging");
 
-						
-			if (prop.getProperty("Wiki_deleteTempFolder").equalsIgnoreCase("true"))
-			{
+			if (prop.getProperty("Wiki_deleteTempFolder").equalsIgnoreCase(
+					"true")) {
 				deleteTempOutputFiles(tempFolder);
 			}
 
-		}
-		catch (Exception e)
-		{
-			System.err.println("Failure in joinTheOutputsAndDeleteTempFilesInTempFolder.");
+		} catch (Exception e) {
+			System.err
+					.println("Failure in joinTheOutputsAndDeleteTempFilesInTempFolder.");
 			e.printStackTrace();
 			System.exit(0);
 		}
 
 	}
 
-	private void mergeDataIntoOneFile(File fileName, File[] fileArray, String filterName) throws Exception
-	{
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fileName), "UTF-8"));
+	private void mergeDataIntoOneFile(File fileName, File[] fileArray,
+			String filterName) throws Exception {
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(
+				new FileOutputStream(fileName), "UTF-8"));
 		BufferedReader br;
 
-		if (filterName.contains("mallet"))
-		{
+		if (filterName.contains("mallet")) {
 			bw.write("\"DOCUMENT_ID\";\"POSITION_OF_TOKEN_IN_DOCUMENT\";\"TERM\";\"TOKEN\";\"WIKI$POSITION\"\n");
 		}
-		
-		for (int i = 0; i < fileArray.length; i++)
-		{
-			if (fileArray[i].getName().contains(filterName))
-			{
-				br = new BufferedReader(new InputStreamReader(new FileInputStream(fileArray[i]), "UTF-8"));
+
+		for (int i = 0; i < fileArray.length; i++) {
+			if (fileArray[i].getName().contains(filterName)) {
+				br = new BufferedReader(new InputStreamReader(
+						new FileInputStream(fileArray[i]), "UTF-8"));
 
 				String line = br.readLine();
-				while (line != null)
-				{
+				while (line != null) {
 
 					bw.write(line);
 					bw.write("\n");
@@ -138,20 +129,15 @@ public class Prehelper
 		bw.close();
 	}
 
-	private void deleteTempOutputFiles(File path)
-	{
-		try
-		{
-			for ( File file : path.listFiles() )
-			    {
-			      if ( file.isDirectory() )
-			        deleteTempOutputFiles( file );
-			      file.delete();
-			    }
-			    path.delete();
-		}
-		catch (Exception e)
-		{
+	private void deleteTempOutputFiles(File path) {
+		try {
+			for (File file : path.listFiles()) {
+				if (file.isDirectory())
+					deleteTempOutputFiles(file);
+				file.delete();
+			}
+			path.delete();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
@@ -159,125 +145,135 @@ public class Prehelper
 	/**
 	 * Testfunktion für Ausgabe
 	 */
-	private void startOnlyOneParsing(Integer old_id) throws InterruptedException, Exception
-	{
+	private void startOnlyOneParsing(Integer old_id)
+			throws InterruptedException, Exception {
 		System.out.println("Start parsing one article.");
-		
+
 		Supporter t = new Supporter(prop);
-		
-		Vector <WikiIDTitlePair> vec = new Vector<WikiIDTitlePair>(1);
-		
-		vec.add( t.getOldIdAndWikiTitleFromWikiPageIdFromDatabase(old_id));
-		
+
+		Vector<WikiIDTitlePair> vec = new Vector<WikiIDTitlePair>(1);
+
+		vec.add(t.getOldIdAndWikiTitleFromWikiPageIdFromDatabase(old_id));
+
 		t.closeDBConnection();
-		
-		Helper h = new Helper(vec,prop,null, "Thread-0");
+
+		Helper h = new Helper(vec, prop, null, "Thread-0");
 		h.setOnlyOneOutputParameter(true);
-		
+
 		h.start();
 		h.join();
 	}
-		
-	private void startWithOffsetNew(Integer limitOrAll, Integer numberOfAvailableProcessors, Integer offset , Integer multiplicator) throws InterruptedException
-	{
+
+	private void startWithOffsetNew(Integer limitOrAll,
+			Integer numberOfAvailableProcessors, Integer offset,
+			Integer multiplicator) throws InterruptedException {
 		ArrayList<WikiIDTitlePair> newList;
-		
-		// get all articles from database 
+
+		// get all articles from database
 		Supporter s = new Supporter(prop);
-		List <WikiIDTitlePair> inputList = s.getArticlesLimitOffset(limitOrAll, offset);
+		List<WikiIDTitlePair> inputList = s.getArticlesLimitOffset(limitOrAll,
+				offset);
 		s.closeDBConnection();
-		
+
 		ThreadGroup group = new ThreadGroup("wikiParsing");
-		
-		
-		//split the whole array into parts
-		ArrayList [] splittedWikiIDTitleArray = s.splitIntoArray(numberOfAvailableProcessors*multiplicator, inputList);
+
+		// split the whole array into parts
+		ArrayList[] splittedWikiIDTitleArray = s.splitIntoArray(
+				numberOfAvailableProcessors * multiplicator, inputList);
 		inputList = null;
-		
-		
-		
-		//declare threadpool
-		Integer listLenght = splittedWikiIDTitleArray.length ;
+
+		// declare threadpool
+		Integer listLenght = splittedWikiIDTitleArray.length;
 		Integer processorMin = numberOfAvailableProcessors;
 		Integer processorMax = numberOfAvailableProcessors;
-		long keepAliveTime = 1; 
+		long keepAliveTime = 1;
 		final LinkedBlockingQueue workQueue = new LinkedBlockingQueue();
-		ThreadPoolExecutor poolExecutor = new ThreadPoolExecutor(processorMin, processorMax, keepAliveTime, TimeUnit.SECONDS, workQueue);
+		ThreadPoolExecutor poolExecutor = new ThreadPoolExecutor(processorMin,
+				processorMax, keepAliveTime, TimeUnit.SECONDS, workQueue);
 		poolExecutor.setCorePoolSize(numberOfAvailableProcessors);
-		
-		//start the threads
-		for (Integer i = 0; i < splittedWikiIDTitleArray.length; i++)
-		{
-			newList = (ArrayList<WikiIDTitlePair>) splittedWikiIDTitleArray[i].clone();
-			poolExecutor.execute(new Helper(newList, prop, group, "threadpart-" + new Integer(i+1) + "of" + listLenght ));
+
+		// start the threads
+		for (Integer i = 0; i < splittedWikiIDTitleArray.length; i++) {
+			newList = (ArrayList<WikiIDTitlePair>) splittedWikiIDTitleArray[i]
+					.clone();
+			poolExecutor.execute(new Helper(newList, prop, group, "threadpart-"
+					+ new Integer(i + 1) + "of" + listLenght));
 			newList = null;
 		}
 		splittedWikiIDTitleArray = null;
-		
-		// wait till the shutdown is finished, otherwise the program tries to join the unfinished outputs 
+
+		// wait till the shutdown is finished, otherwise the program tries to
+		// join the unfinished outputs
 		poolExecutor.shutdown();
-		while (!poolExecutor.awaitTermination(30, TimeUnit.SECONDS))
-		{
-//			do nothing
+		while (!poolExecutor.awaitTermination(30, TimeUnit.SECONDS)) {
+			// do nothing
 		}
 		poolExecutor = null;
 	}
 
-	
-	public void start()
-	{
-		
-		try
-		{			
+	public void start() {
+
+		try {
 			Integer limit, offset, old_id, numberOfAvailableProcesssors, multiplicator;
-		
-			Prehelper p = new Prehelper(prop);		
-			
+
+			Prehelper p = new Prehelper(prop);
+
 			String functionName = "parsing of wiki-articles incl joining the outputs";
 			p.stopWatch.startStopping(functionName);
 
-			//read properties
-			numberOfAvailableProcesssors = Integer.valueOf((String) prop.get("Wiki_numberOfParallelThreads"));
-			limit = Integer.valueOf(prop.getProperty("Wiki_limit"));
-			offset = Integer.valueOf(prop.getProperty("Wiki_offset"));
-			
-			try
-			{
-				multiplicator = Integer.valueOf(prop.getProperty("Wiki_multiplicatorForNumberOfThreads"));
-				old_id = Integer.valueOf(prop.getProperty("Wiki_old_id"));
+			// read properties
+
+			try {
+				numberOfAvailableProcesssors = Integer.valueOf((String) prop
+						.get("Wiki_numberOfParallelThreads"));
+			} catch (NumberFormatException e2) {
+				numberOfAvailableProcesssors = 1;
 			}
-			catch (NumberFormatException e)
-			{
-				System.err.println("perhaps a failure in get property, but it doesn't matter, optinal multiplicator is set to two and old_id to zero");
+
+			try {
+				limit = Integer.valueOf(prop.getProperty("Wiki_limit"));
+			} catch (Exception e1) {
+
+				limit = 0; // bedeutet alles
+			}
+			try {
+				offset = Integer.valueOf(prop.getProperty("Wiki_offset"));
+			} catch (NumberFormatException e1) {
+				offset = 0;
+			}
+
+			try {
+				multiplicator = Integer.valueOf(prop
+						.getProperty("Wiki_multiplicatorForNumberOfThreads"));
+
+			} catch (NumberFormatException e) {
 				multiplicator = 2;
+			}
+
+			try {
+				old_id = Integer.valueOf(prop.getProperty("Wiki_old_id"));
+			} catch (NumberFormatException e) {
 				old_id = 0;
 			}
-			
-//			false bedeutet, dass die csv und sql auf den geparsten Text aufbauen			
-//			true bedeutet, dass die csv vom Orignaltext erstellt wird, und originalem Input
-//			offset == -1 bedeutet random Artikel
 
-////mainpart			
-			// für Testzwecke, kann zusammengeschrupft oder mit extra Option gemacht werden
-			if (old_id > 0)
-			{
+			// //mainpart
+			// für Testzwecke, kann zusammengeschrupft oder mit extra Option
+			// gemacht werden
+			if (old_id > 0) {
 				p.startOnlyOneParsing(old_id);
-			}
-			else
-			{
-				p.startWithOffsetNew(limit, numberOfAvailableProcesssors, offset,multiplicator);
+			} else {
+				p.startWithOffsetNew(limit, numberOfAvailableProcesssors,
+						offset, multiplicator);
 			}
 
-			
 			p.joinTheOutputsAndDeleteTempFilesInTempFolder();
 			p.stopWatch.stopStoppingAndDoOutputToConsole(functionName);
-			
-		}
-		catch (Exception e)
-		{
+
+		} catch (Exception e) {
 			System.err.println("Prehelper:");
-//			logger.error("prehelper " + e.getMessage() + " " + e.getStackTrace());
+			// logger.error("prehelper " + e.getMessage() + " " +
+			// e.getStackTrace());
 			e.printStackTrace();
 		}
-	}	
+	}
 }

c7f61a4306a377651051cb6060b73018a492e2ea
Mattes Angelus <mattes.angelus@japanologie.uni-halle.de>
2013-11-08 19:08:25 +0000

    bugfix Prune_db - hat mit TOKEN statt TERM gerechnet

--- plugin-prune-preprocessing/src/main/java/cc/topicexplorer/plugin/prune/preprocessing/tools/Prune_db.java
+++ plugin-prune-preprocessing/src/main/java/cc/topicexplorer/plugin/prune/preprocessing/tools/Prune_db.java
@@ -75,21 +75,6 @@ public class Prune_db extends DependencyCommand {
 					System.exit(0);
 				}
 
-				ResultSet rsDocCount = database.executeQuery("SELECT COUNT(*) FROM "
-						+ properties.getProperty("OrgTableName"));
-				if (rsDocCount.next()) {
-					int count = rsDocCount.getInt(1);
-					upperBound = (float) (count / 100.0) * upperBoundPercent;
-					lowerBound = (float) (count / 100.0) * lowerBoundPercent;
-				} else {
-					lowerBound = 0.0f;
-					upperBound = Float.MAX_VALUE;
-				}
-
-				PrintWriter writer = new PrintWriter(new OutputStreamWriter(new BufferedOutputStream(
-						new FileOutputStream(properties.getProperty("InCSVFile") + ".pruned.Lower." + lowerBound
-								+ ".Upper." + upperBound + ".csv")), "UTF-8"));
-
 				// copy from doctermtopic and delete topic_id
 				/**
 				 * MIT-JOOQ-START database.executeUpdateQuery(
@@ -97,22 +82,39 @@ public class Prune_db extends DependencyCommand {
 				 * DOCUMENT_TERM_TOPIC.getName()); MIT-JOOQ-ENDE
 				 */
 				/** OHNE_JOOQ-START */
-				database.executeUpdateQuery("CREATE TABLE IF NOT EXISTS TEMP4PRUNE LIKE " + "DOCUMENT_TERM_TOPIC");
+				database.executeUpdateQuery("CREATE TABLE IF NOT EXISTS TEMP4PRUNE LIKE DOCUMENT_TERM_TOPIC");
 				/** OHNE_JOOQ-ENDE */
 				database.executeUpdateQuery("ALTER TABLE TEMP4PRUNE DROP COLUMN TOPIC_ID");
 
 				database.executeUpdateQuery("LOAD DATA LOCAL INFILE '" + inFilePath + "' IGNORE INTO TABLE TEMP4PRUNE "
-						+ "CHARACTER SET UTF8 " + "FIELDS TERMINATED BY ';' ENCLOSED BY '\"' IGNORE 1 LINES (" + select
+						+ "CHARACTER SET UTF8 FIELDS TERMINATED BY ';' ENCLOSED BY '\"' IGNORE 1 LINES (" + select
 						+ ");");
 
-				database.executeUpdateQuery("CREATE TABLE TEMP4PRUNE2 (UNIQUE (TOKEN)) "
-						+ "ENGINE=MEMORY AS SELECT TOKEN FROM TEMP4PRUNE GROUP BY TOKEN "
+				ResultSet rsDocCount = database.executeQuery("SELECT COUNT(DISTINCT DOCUMENT_ID) FROM TEMP4PRUNE");
+				int count = 0;
+				if (rsDocCount.next()) {
+					count = rsDocCount.getInt(1);
+					upperBound = (float) (count / 100.0) * upperBoundPercent;
+					lowerBound = (float) (count / 100.0) * lowerBoundPercent;
+				} else {
+					lowerBound = 0.0f;
+					upperBound = Float.MAX_VALUE;
+				}
+
+				logger.info("Pruning: count: " + count + " lower: " + lowerBound + " upper: " + upperBound);
+				PrintWriter writer = new PrintWriter(new OutputStreamWriter(new BufferedOutputStream(
+						new FileOutputStream(properties.getProperty("InCSVFile") + ".pruned.Lower." + lowerBound
+								+ ".Upper." + upperBound + ".csv")), "UTF-8"));
+
+				database.executeUpdateQuery("CREATE TABLE TEMP4PRUNE2 (UNIQUE (TERM)) "
+						+ "ENGINE=MEMORY AS SELECT TERM FROM TEMP4PRUNE GROUP BY TERM "
 						+ "HAVING COUNT(DISTINCT DOCUMENT_ID) < " + upperBound + " AND COUNT(DISTINCT DOCUMENT_ID) > "
 						+ lowerBound);
 
 				ResultSet prunedRS = database.executeQuery("SELECT " + select + " FROM " + "TEMP4PRUNE,TEMP4PRUNE2 "
-						+ "WHERE TEMP4PRUNE.TOKEN=TEMP4PRUNE2.TOKEN "
+						+ "WHERE TEMP4PRUNE.TERM=TEMP4PRUNE2.TERM "
 						+ "ORDER BY DOCUMENT_ID,POSITION_OF_TOKEN_IN_DOCUMENT");
+
 				ResultSetMetaData md = prunedRS.getMetaData();
 
 				writer.append(header).println();
@@ -136,6 +138,7 @@ public class Prune_db extends DependencyCommand {
 					writer.append(line).println();
 				}
 				writer.close();
+
 				database.executeUpdateQuery("DROP TABLE TEMP4PRUNE;");
 				database.executeUpdateQuery("DROP TABLE TEMP4PRUNE2;");
 

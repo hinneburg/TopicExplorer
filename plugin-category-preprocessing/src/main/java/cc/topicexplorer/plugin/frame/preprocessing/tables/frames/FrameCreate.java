ec2d9dc0445e0da97c65cbca5d4355b3f575e54f
Benjamin Schandera <4.32@gmx.de>
2013-12-07 21:17:02 +0000

    #151 lege Projektstruktur für neues Plugin an

---
+++ plugin-category-preprocessing/src/main/java/cc/topicexplorer/plugin/frame/preprocessing/tables/frames/FrameCreate.java
@@ -0,0 +1,21 @@
+package cc.topicexplorer.plugin.frame.preprocessing.tables.frames;
+
+import java.sql.SQLException;
+
+import cc.topicexplorer.chain.commands.TableCreateCommand;
+
+public final class FrameCreate extends TableCreateCommand {
+
+	@Override
+	public void createTable() throws SQLException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setTableName() {
+		// TODO Auto-generated method stub
+
+	}
+
+}

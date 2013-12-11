6133f2d34150107a4c9884b36d3edd12d2491923
Benjamin Schandera <4.32@gmx.de>
2013-12-11 16:04:34 +0000

    #151 ermögliche das Erstellen der Frames Tabelle

--- plugin-frame-preprocessing/src/main/java/cc/topicexplorer/plugin/frame/preprocessing/tables/frames/FrameCreate.java
+++ plugin-frame-preprocessing/src/main/java/cc/topicexplorer/plugin/frame/preprocessing/tables/frames/FrameCreate.java
@@ -1,18 +1,27 @@
 package cc.topicexplorer.plugin.frame.preprocessing.tables.frames;
 
+import java.sql.SQLException;
+
 import cc.topicexplorer.chain.commands.TableCreateCommand;
+
+import com.google.common.base.Preconditions;
 
 public final class FrameCreate extends TableCreateCommand {
 
 	@Override
 	public void createTable() {
-	}
-
-	@Override
-	public void dropTable() {
+		Preconditions.checkState(this.tableName != null, "Table name is not set, yet");
+		try {
+			this.database.executeUpdateQuery("CREATE TABLE " + this.tableName
+					+ " (DOCUMENT_ID INT, TOPIC_ID INT, FRAME VARCHAR(255), START_POSITION INT, END_POSITION INT)");
+		} catch (SQLException e) {
+			this.logger.error("Table Frames could not be created.");
+			throw new RuntimeException(e);
+		}
 	}
 
 	@Override
 	public void setTableName() {
+		this.tableName = "Frames";
 	}
 }
--- plugin-frame-preprocessing/src/test/java/cc/topicexplorer/plugin/frame/preprocessing/tables/frames/FrameCreateTest.java
+++ plugin-frame-preprocessing/src/test/java/cc/topicexplorer/plugin/frame/preprocessing/tables/frames/FrameCreateTest.java
@@ -1,5 +1,42 @@
 package cc.topicexplorer.plugin.frame.preprocessing.tables.frames;
 
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.verify;
+
+import java.sql.SQLException;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import cc.topicexplorer.database.Database;
+
 public class FrameCreateTest {
 
+	@Mock(name = "database")
+	private Database dbMock;
+
+	@InjectMocks
+	FrameCreate frameCreate = new FrameCreate();
+
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+	}
+
+	@Test
+	public void testCreateTable() throws SQLException {
+		this.frameCreate.setTableName();
+		this.frameCreate.createTable();
+		verify(this.dbMock).executeUpdateQuery(
+				eq("CREATE TABLE Frames "
+						+ "(DOCUMENT_ID INT, TOPIC_ID INT, FRAME VARCHAR(255), START_POSITION INT, END_POSITION INT)"));
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void testCreateTableThrowsException() throws SQLException {
+		this.frameCreate.createTable();
+	}
 }

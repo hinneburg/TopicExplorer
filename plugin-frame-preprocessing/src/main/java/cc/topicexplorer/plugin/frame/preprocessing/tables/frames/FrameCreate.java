4829d4c4ac1e20d3bf1b2c95c4c0297f10ecb8b7
Mattes Angelus <mattes.angelus@japanologie.uni-halle.de>
2013-12-16 14:20:18 +0000

    Feld FRAME_ID hinzugefuegt

--- plugin-frame-preprocessing/src/main/java/cc/topicexplorer/plugin/frame/preprocessing/tables/frames/FrameCreate.java
+++ plugin-frame-preprocessing/src/main/java/cc/topicexplorer/plugin/frame/preprocessing/tables/frames/FrameCreate.java
@@ -13,7 +13,7 @@ public final class FrameCreate extends TableCreateCommand {
 		Preconditions.checkState(this.tableName != null, "Table name has not been set, yet");
 		try {
 			this.database.executeUpdateQuery("CREATE TABLE " + this.tableName
-					+ " (DOCUMENT_ID INT, TOPIC_ID INT, FRAME VARCHAR(255), START_POSITION INT, END_POSITION INT) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;");
+					+ " (FRAME_ID INT NOT NULL AUTO_INCREMENT, DOCUMENT_ID INT, TOPIC_ID INT, FRAME VARCHAR(255), START_POSITION INT, END_POSITION INT,PRIMARY KEY (FRAME_ID)) DEFAULT CHARSET=utf8 COLLATE=utf8_bin");
 		} catch (SQLException e) {
 			this.logger.error("Table " + this.tableName + " could not be created.");
 			throw new RuntimeException(e);

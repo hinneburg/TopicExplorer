8b82fadafbf8a85d4aabd87b8dfe7acd783b77c7
Benjamin Schandera <4.32@gmx.de>
2014-01-08 15:31:40 +0000

    #151 korrigiere falschen Tabellennamen

--- plugin-frame-preprocessing/src/test/java/cc/topicexplorer/plugin/frame/preprocessing/tables/frames/FrameCreateTest.java
+++ plugin-frame-preprocessing/src/test/java/cc/topicexplorer/plugin/frame/preprocessing/tables/frames/FrameCreateTest.java
@@ -6,7 +6,6 @@ import static org.mockito.Mockito.verify;
 import java.sql.SQLException;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -26,30 +25,19 @@ public class FrameCreateTest {
 	public void setup() {
 		MockitoAnnotations.initMocks(this);
 	}
-	@Ignore
+
 	@Test
 	public void testCreateTable() throws SQLException {
 		this.frameCreate.setTableName();
 		this.frameCreate.createTable();
-		verify(this.dbMock).executeUpdateQuery(
-				eq("CREATE TABLE Frames "
-						+ "(DOCUMENT_ID INT, TOPIC_ID INT, FRAME VARCHAR(255), START_POSITION INT, END_POSITION INT)"));
+		verify(this.dbMock)
+				.executeUpdateQuery(
+						eq("CREATE TABLE FRAMES (FRAME_ID INT NOT NULL AUTO_INCREMENT, DOCUMENT_ID INT, TOPIC_ID INT, FRAME VARCHAR(255), START_POSITION INT, END_POSITION INT,PRIMARY KEY (FRAME_ID)) DEFAULT CHARSET=utf8 COLLATE=utf8_bin"));
 	}
-	@Ignore
+
 	@Test(expected = IllegalStateException.class)
 	public void testCreateTableThrowsException() throws SQLException {
 		this.frameCreate.createTable();
 	}
-	@Ignore
-	@Test
-	public void testDropTable() throws SQLException {
-		this.frameCreate.setTableName();
-		this.frameCreate.dropTable();
-		verify(this.dbMock).executeUpdateQuery("DROP TABLE Frames");
-	}
-	@Ignore
-	@Test(expected = IllegalStateException.class)
-	public void testDropTableThrowsException() throws SQLException {
-		this.frameCreate.dropTable();
-	}
+
 }

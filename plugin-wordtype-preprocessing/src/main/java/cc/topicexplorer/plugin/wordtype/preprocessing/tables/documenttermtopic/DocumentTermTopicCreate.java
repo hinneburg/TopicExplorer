2331ea785a2692d4ce6d65407914b98e70a852c2
Benjamin Schandera <4.32@gmx.de>
2014-01-22 21:45:16 +0000

    FEHLERB. (wordtype-preprocessing): - Kommandos entscheiden selbständig, ob sie erfolgreich abgearbeitet wurden - Checked Exceptions aus Methodensignatur entfernen - kleinere Überarbeitungen

--- plugin-wordtype-preprocessing/src/main/java/cc/topicexplorer/plugin/wordtype/preprocessing/tables/documenttermtopic/DocumentTermTopicCreate.java
+++ plugin-wordtype-preprocessing/src/main/java/cc/topicexplorer/plugin/wordtype/preprocessing/tables/documenttermtopic/DocumentTermTopicCreate.java
@@ -11,19 +11,27 @@ import cc.topicexplorer.chain.commands.TableCreateCommand;
 public class DocumentTermTopicCreate extends TableCreateCommand {
 
 	@Override
-	public void createTable() throws SQLException {
-		database.executeUpdateQuery("ALTER IGNORE TABLE `" + this.tableName
-				+ "` ADD COLUMN WORDTYPE$WORDTYPE varchar(255) COLLATE utf8_bin NOT NULL;");
+	public void createTable() {
+		try {
+			database.executeUpdateQuery("ALTER IGNORE TABLE `" + this.tableName
+					+ "` ADD COLUMN WORDTYPE$WORDTYPE varchar(255) COLLATE utf8_bin NOT NULL;");
+		} catch (SQLException e) {
+			logger.error("Column WORDTYPE$WORDTYPE could not be added to table " + this.tableName);
+			throw new RuntimeException(e);
+		}
 	}
 
 	@Override
-	public void dropTable() throws SQLException {
-		// database.dropTable(this.tableName);
+	public void dropTable() {
 		try {
-			this.database.executeUpdateQuery("ALTER TABLE " + this.tableName
-					+ " DROP COLUMN WORDTYPE$WORDTYPE");
-		} catch (Exception e) {
-			logger.warn("Document.dropColumns: Cannot drop column, perhaps it doesn't exists. Doesn't matter ;)");
+			this.database.executeUpdateQuery("ALTER TABLE " + this.tableName + " DROP COLUMN WORDTYPE$WORDTYPE");
+		} catch (SQLException e) {
+			if (e.getErrorCode() != 1091) { // MySQL Error code for: 'Can't //
+											// DROP..; check that column/key //
+											// exists
+				logger.error("Document.dropColumns: Cannot drop column.");
+				throw new RuntimeException(e);
+			}
 
 		}
 	}
@@ -32,10 +40,10 @@ public class DocumentTermTopicCreate extends TableCreateCommand {
 	public void setTableName() {
 		tableName = "DOCUMENT_TERM_TOPIC";
 	}
-	
+
 	@Override
 	public void addDependencies() {
 		beforeDependencies.add("DocumentTermTopicCreate");
 		afterDependencies.add("InFilePreparation");
-	}	
+	}
 }

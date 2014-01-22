364bde0c4a41a271000c872dfa0a8e4d4a881665
Benjamin Schandera <4.32@gmx.de>
2014-01-22 21:52:01 +0000

    Kleine Überarbeitung: Entferne doppelte Logger-Meldung

--- command-manager/src/main/java/cc/topicexplorer/chain/commands/DbConnectionCommand.java
+++ command-manager/src/main/java/cc/topicexplorer/chain/commands/DbConnectionCommand.java
@@ -3,20 +3,14 @@ package cc.topicexplorer.chain.commands;
 import java.util.Properties;
 
 import org.apache.commons.chain.Context;
-import org.apache.log4j.Logger;
 
 import cc.topicexplorer.chain.CommunicationContext;
 import cc.topicexplorer.database.Database;
 
 public class DbConnectionCommand extends DependencyCommand {
 
-	private final Logger logger = Logger.getRootLogger();
-
 	@Override
 	public void specialExecute(Context context) {
-
-		logger.info("Current Command : [ " + getClass() + " ]");
-
 		CommunicationContext communicationContext = (CommunicationContext) context;
 		Database database = new Database((Properties) communicationContext.get("properties"), false);
 		communicationContext.put("database", database);

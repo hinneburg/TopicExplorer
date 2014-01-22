c64d6b4fd742b5c0f56e5d8603a7df61df9a9224
Benjamin Schandera <4.32@gmx.de>
2014-01-22 20:49:08 +0000

    FEHLERB. (colortopic-preprocessing): - Kommandos entscheiden selbständig, ob sie erfolgreich abgearbeitet wurden - Checked Exceptions aus Methodensignatur entfernen - kleinere Überarbeitungen

--- plugin-colortopic-preprocessing/src/main/java/cc/topicexplorer/plugin/colortopic/preprocessing/tables/topic/TopicCreate.java
+++ plugin-colortopic-preprocessing/src/main/java/cc/topicexplorer/plugin/colortopic/preprocessing/tables/topic/TopicCreate.java
@@ -4,47 +4,49 @@ import java.sql.SQLException;
 
 import cc.topicexplorer.chain.commands.TableCreateCommand;
 
-/**
+/*
  * angefangen von Mattes weiterverarbeitet von Gert Kommaersetzung, Pfadangabe
  * eingefügt, Tabellenname mit Jooq verknüpft
  * 
  */
 public class TopicCreate extends TableCreateCommand {
 
-	public void createTable() throws SQLException {
-		this.database
-				.executeUpdateQuery("ALTER TABLE "
-						+ this.tableName
-						+ " ADD COLUMN TOPIC.COLOR_TOPIC$COLOR VARCHAR(10) COLLATE utf8_bin");
+	@Override
+	public void createTable() {
+		try {
+			this.database.executeUpdateQuery("ALTER TABLE " + this.tableName
+					+ " ADD COLUMN TOPIC.COLOR_TOPIC$COLOR VARCHAR(10) COLLATE utf8_bin");
+		} catch (SQLException e) {
+			logger.error("Column TOPIC.COLOR_TOPIC$COLOR could not be added to table " + this.tableName);
+			throw new RuntimeException(e);
+		}
 	}
 
-	private void dropColumns() throws SQLException {
-
+	private void dropColumns() {
 		try {
-			this.database
-					.executeUpdateQuery("ALTER TABLE "
-							+ this.tableName
-							+ " DROP COLUMN TOPIC.COLOR_TOPIC$COLOR");
-		} catch (Exception e) {
-			logger.warn("TopicMetaData.dropColumns: Cannot drop column, perhaps it doesn't exists. Doesn't matter ;)");
-
+			this.database.executeUpdateQuery("ALTER TABLE " + this.tableName + " DROP COLUMN TOPIC.COLOR_TOPIC$COLOR");
+		} catch (SQLException e) {
+			if (e.getErrorCode() != 1091) { // MySQL Error code for 'Can't DROP
+				// ..; check that column/key exists
+				logger.error("TopicMetaData.dropColumns: Cannot drop column.");
+				throw new RuntimeException(e);
+			}
 		}
 	}
 
 	@Override
-	public void dropTable() throws SQLException {
-		 this.dropColumns();
+	public void dropTable() {
+		this.dropColumns();
 	}
 
 	@Override
 	public void setTableName() {
 		this.tableName = "TOPIC";
 	}
-	
+
 	@Override
 	public void addDependencies() {
 		beforeDependencies.add("TopicCreate");
-	}	
-	
+	}
 
 }
--- plugin-colortopic-preprocessing/src/main/java/cc/topicexplorer/plugin/colortopic/preprocessing/tables/topic/TopicFill.java
+++ plugin-colortopic-preprocessing/src/main/java/cc/topicexplorer/plugin/colortopic/preprocessing/tables/topic/TopicFill.java
@@ -1,48 +1,51 @@
 package cc.topicexplorer.plugin.colortopic.preprocessing.tables.topic;
+
 /** MIT-JOOQ-START 
-import static jooq.generated.Tables.TOPIC;
-MIT-JOOQ-ENDE */ 
+ import static jooq.generated.Tables.TOPIC;
+ MIT-JOOQ-ENDE */
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-
 import cc.topicexplorer.chain.commands.TableFillCommand;
 
 /**
- * angefangen von Mattes weiterverarbeitet von Gert Kommaersetzung, Pfadangabe
- * eingefügt, Tabellenname mit Jooq verknüpft
+ * @autor angefangen von Mattes weiterverarbeitet von Gert Kommaersetzung,
+ *        Pfadangabe eingefügt, Tabellenname mit Jooq verknüpft
  * 
  */
 public class TopicFill extends TableFillCommand {
 
-	public void fillTable() throws SQLException {
+	@Override
+	public void fillTable() {
 		try {
 			this.prepareMetaDataAndFillTable();
-		} catch (IOException e) {
-			e.printStackTrace();
+		} catch (IOException ioEx) {
+			logger.error("Problems occured while reading temp/topic_order.csv");
+			throw new RuntimeException(ioEx);
+		} catch (SQLException sqlEx) {
+			logger.error("Table " + this.tableName + " could not be filled properly.");
+			throw new RuntimeException(sqlEx);
 		}
 	}
-
-	
 
 	private void prepareMetaDataAndFillTable() throws IOException, SQLException {
 		FileReader fr = new FileReader("temp/topic_order.csv");
 		BufferedReader br = new BufferedReader(fr);
 
 		String id;
-		/** MIT-JOOQ-START 
-		Statement stmt = database.getCreateJooq().getConnection()
-				.createStatement();
-		MIT-JOOQ-ENDE */ 
-		/** OHNE_JOOQ-START */ 
+		/**
+		 * MIT-JOOQ-START Statement stmt =
+		 * database.getCreateJooq().getConnection() .createStatement();
+		 * MIT-JOOQ-ENDE
+		 */
+		/** OHNE_JOOQ-START */
 		Statement stmt = this.database.getConnection().createStatement();
-		/** OHNE_JOOQ-ENDE */ 
-		for (int i = 0; i < Integer.parseInt(this.properties
-				.getProperty("malletNumTopics")); i++) {
-		
+		/** OHNE_JOOQ-ENDE */
+		for (int i = 0; i < Integer.parseInt(this.properties.getProperty("malletNumTopics")); i++) {
+
 			String[] oldLine = br.readLine().split(",");
 
 			id = Integer.toString(Integer.parseInt(oldLine[0]) - 1);
@@ -52,41 +55,43 @@ public class TopicFill extends TableFillCommand {
 			if (colour.endsWith("FF") && colour.length() == 9) {
 				colour = colour.substring(0, 7);
 			}
-		
-			/** MIT-JOOQ-START 
-			stmt.addBatch(" UPDATE " + TOPIC.getName() + " set "
-						+ TOPIC.COLOR_TOPIC$COLOR.getName() + " = '" + colour
-						+ "' WHERE "
-						+ TOPIC.TOPIC_ID.getName() + " = " + id + "; ");
-			MIT-JOOQ-ENDE */ 	
-			/** OHNE_JOOQ-START */ 
+
+			/**
+			 * MIT-JOOQ-START stmt.addBatch(" UPDATE " + TOPIC.getName() +
+			 * " set " + TOPIC.COLOR_TOPIC$COLOR.getName() + " = '" + colour +
+			 * "' WHERE " + TOPIC.TOPIC_ID.getName() + " = " + id + "; ");
+			 * MIT-JOOQ-ENDE
+			 */
+			/** OHNE_JOOQ-START */
+//			@formatter:off
 			stmt.addBatch(" UPDATE " + "TOPIC" + " set "
 					+ "TOPIC.COLOR_TOPIC$COLOR" + " = '" + colour
 					+ "' WHERE "
 					+ "TOPIC.TOPIC_ID" + " = " + id + "; ");
-			/** OHNE_JOOQ-ENDE */ 
+//			@formatter:on
+			/** OHNE_JOOQ-ENDE */
 		}
 
 		logger.info("Starting batch execution TopicMetaData update themen.");
 		stmt.executeBatch();
 		br.close();
-		
+
 	}
 
 	@Override
 	public void setTableName() {
-	/** MIT-JOOQ-START 
-		this.tableName = TOPIC.getName();
-	MIT-JOOQ-ENDE */ 
-	/** OHNE_JOOQ-START */ 	
+		/**
+		 * MIT-JOOQ-START this.tableName = TOPIC.getName(); MIT-JOOQ-ENDE
+		 */
+		/** OHNE_JOOQ-START */
 		this.tableName = "TOPIC";
-	/** OHNE_JOOQ-ENDE */ 
+		/** OHNE_JOOQ-ENDE */
 	}
-	
+
 	@Override
 	public void addDependencies() {
 		beforeDependencies.add("TopicFill");
 		beforeDependencies.add("TopicMetaData");
 		beforeDependencies.add("ColorTopic_TopicCreate");
-	}	
+	}
 }

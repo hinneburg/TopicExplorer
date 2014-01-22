bf9da50688de355f6bca677e21b63082ea124d8a
Benjamin Schandera <4.32@gmx.de>
2014-01-22 20:47:18 +0000

    FEHLERB. (category-preprocessing): - Kommandos entscheiden selbständig, ob sie erfolgreich abgearbeitet wurden - Checked Exceptions aus Methodensignatur entfernen - kleinere Überarbeitungen

--- plugin-category-preprocessing/src/main/java/cc/topicexplorer/plugin/category/preprocessing/tables/document/DocumentCreate.java
+++ plugin-category-preprocessing/src/main/java/cc/topicexplorer/plugin/category/preprocessing/tables/document/DocumentCreate.java
@@ -7,31 +7,36 @@ import cc.topicexplorer.chain.commands.TableCreateCommand;
 public class DocumentCreate extends TableCreateCommand {
 
 	@Override
-	public void createTable() throws SQLException {
-
-		database.executeUpdateQuery("ALTER IGNORE TABLE " + this.tableName 
-				+ " ADD COLUMN CATEGORY$CATEGORY_ID INTEGER(11) NOT NULL ");
-	}
-
-	@Override
-	public void dropTable() throws SQLException {
+	public void createTable() {
 		try {
-			database.executeUpdateQuery("ALTER IGNORE TABLE " + this.tableName 
-						+ " DROP COLUMN CATEGORY$CATEGORY_ID");
-		} catch (Exception e) {
-			logger.warn("Document.dropColumns: Cannot drop column, perhaps it doesn't exists. Doesn't matter ;)");
-
+			database.executeUpdateQuery("ALTER IGNORE TABLE " + this.tableName
+					+ " ADD COLUMN CATEGORY$CATEGORY_ID INTEGER(11) NOT NULL ");
+		} catch (SQLException e) {
+			logger.error("Column CATEGORY$CATEGORY_ID could not be added to table " + this.tableName);
+			throw new RuntimeException(e);
 		}
 	}
 
+	@Override
+	public void dropTable() {
+		try {
+			database.executeUpdateQuery("ALTER IGNORE TABLE " + this.tableName + " DROP COLUMN CATEGORY$CATEGORY_ID");
+		} catch (SQLException e) {
+			if (e.getErrorCode() != 1091) { // MySQL Error code for 'Can't DROP
+											// ..; check that column/key exists
+				logger.error("Document.dropColumns: Cannot drop column.");
+				throw new RuntimeException(e);
+			}
+		}
+	}
 
 	@Override
 	public void setTableName() {
 		tableName = "DOCUMENT";
 	}
-	
+
 	@Override
 	public void addDependencies() {
 		beforeDependencies.add("DocumentCreate");
-	}	
+	}
 }
--- plugin-category-preprocessing/src/main/java/cc/topicexplorer/plugin/category/preprocessing/tables/document/DocumentFill.java
+++ plugin-category-preprocessing/src/main/java/cc/topicexplorer/plugin/category/preprocessing/tables/document/DocumentFill.java
@@ -1,64 +1,63 @@
 package cc.topicexplorer.plugin.category.preprocessing.tables.document;
 
 import java.sql.SQLException;
-
 /** MIT-JOOQ-START 
-import static jooq.generated.Tables.DOCUMENT; 
-MIT-JOOQ-ENDE */ 
-
+ import static jooq.generated.Tables.DOCUMENT; 
+ MIT-JOOQ-ENDE */
 import cc.topicexplorer.chain.commands.TableFillCommand;
 
 public class DocumentFill extends TableFillCommand {
 
 	@Override
-	public void fillTable() throws SQLException {
-	/** MIT-JOOQ-START 
+	public void fillTable() {
+		/**
+		 * MIT-JOOQ-START if
+		 * (Boolean.parseBoolean(properties.getProperty("plugin_category"))) {
+		 * database.executeUpdateQuery("UPDATE " + this.tableName + " d, " +
+		 * properties.getProperty("OrgTableName") + " org SET d." +
+		 * DOCUMENT.CATEGORY$CATEGORY_ID.getName() + " = org." +
+		 * properties.getProperty("Category_OrgTableCat") + " WHERE d." +
+		 * DOCUMENT.DOCUMENT_ID.getName() + " = org." +
+		 * properties.getProperty("OrgTableId"));
+		 * 
+		 * 
+		 * database.executeUpdateQuery("ALTER IGNORE TABLE " + this.tableName +
+		 * " ADD KEY CATEGORY_DOCUMENT_IDX (`" + DOCUMENT.DOCUMENT_ID.getName()
+		 * + "`,`" + DOCUMENT.CATEGORY$CATEGORY_ID.getName() + "`) "); }
+		 * MIT-JOOQ-ENDE
+		 */
+		/** OHNE_JOOQ-START */
 		if (Boolean.parseBoolean(properties.getProperty("plugin_category"))) {
-			database.executeUpdateQuery("UPDATE " + this.tableName + " d, "
-					+ properties.getProperty("OrgTableName") + " org SET d."
-					+ DOCUMENT.CATEGORY$CATEGORY_ID.getName() + " = org."
-					+ properties.getProperty("Category_OrgTableCat") 					
-					+ " WHERE d." + DOCUMENT.DOCUMENT_ID.getName() + " = org."
-					+ properties.getProperty("OrgTableId"));
-			
-	
-			database.executeUpdateQuery("ALTER IGNORE TABLE " + this.tableName
-					+ " ADD KEY CATEGORY_DOCUMENT_IDX (`" 
-					+ DOCUMENT.DOCUMENT_ID.getName() 
-					+ "`,`" + DOCUMENT.CATEGORY$CATEGORY_ID.getName() + "`) ");
+			try {
+				database.executeUpdateQuery("UPDATE " + this.tableName + " d, "
+						+ properties.getProperty("OrgTableName") + " org SET d." + "DOCUMENT.CATEGORY$CATEGORY_ID"
+						+ " = org." + properties.getProperty("Category_OrgTableCat") + " WHERE d."
+						+ "DOCUMENT.DOCUMENT_ID" + " = org." + properties.getProperty("OrgTableId"));
+
+				database.executeUpdateQuery("ALTER IGNORE TABLE " + this.tableName
+						+ " ADD KEY CATEGORY_DOCUMENT_IDX (`" + "DOCUMENT.DOCUMENT_ID" + "`,`"
+						+ "DOCUMENT.CATEGORY$CATEGORY_ID" + "`) ");
+			} catch (SQLException e) {
+				logger.error("Table " + this.tableName + " could not be filled properly.");
+				throw new RuntimeException(e);
+			}
 		}
-		MIT-JOOQ-ENDE */ 
-		/** OHNE_JOOQ-START */ 
-		if (Boolean.parseBoolean(properties.getProperty("plugin_category"))) {
-			database.executeUpdateQuery("UPDATE " + this.tableName + " d, "
-					+ properties.getProperty("OrgTableName") + " org SET d."
-					+ "DOCUMENT.CATEGORY$CATEGORY_ID" + " = org."
-					+ properties.getProperty("Category_OrgTableCat") 					
-					+ " WHERE d." + "DOCUMENT.DOCUMENT_ID" + " = org."
-					+ properties.getProperty("OrgTableId"));
-			
-	
-			database.executeUpdateQuery("ALTER IGNORE TABLE " + this.tableName
-					+ " ADD KEY CATEGORY_DOCUMENT_IDX (`" 
-					+ "DOCUMENT.DOCUMENT_ID" 
-					+ "`,`" + "DOCUMENT.CATEGORY$CATEGORY_ID" + "`) ");
-		}
-		/** OHNE_JOOQ-ENDE */ 
+		/** OHNE_JOOQ-ENDE */
 	}
 
 	@Override
 	public void setTableName() {
-	/** MIT-JOOQ-START 
-		tableName = DOCUMENT.getName();
-	MIT-JOOQ-ENDE */ 
-		/** OHNE_JOOQ-START */ 
+		/**
+		 * MIT-JOOQ-START tableName = DOCUMENT.getName(); MIT-JOOQ-ENDE
+		 */
+		/** OHNE_JOOQ-START */
 		tableName = "DOCUMENT";
-		/** OHNE_JOOQ-ENDE */ 
+		/** OHNE_JOOQ-ENDE */
 	}
 
 	@Override
 	public void addDependencies() {
 		beforeDependencies.add("DocumentFill");
 		beforeDependencies.add("Category_DocumentCreate");
-	}	
+	}
 }

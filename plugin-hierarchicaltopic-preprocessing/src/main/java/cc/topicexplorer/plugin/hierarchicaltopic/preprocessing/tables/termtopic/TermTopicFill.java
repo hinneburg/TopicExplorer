be8ebdb74344fda3da60aa5627fd335c1864b46c
Benjamin Schandera <4.32@gmx.de>
2014-01-22 20:51:48 +0000

    FEHLERB. (hierarchicaltopic-preprocessing): - Kommandos entscheiden selbständig, ob sie erfolgreich abgearbeitet wurden - Checked Exceptions aus Methodensignatur entfernen - kleinere Überarbeitungen

--- plugin-hierarchicaltopic-preprocessing/src/main/java/cc/topicexplorer/plugin/hierarchicaltopic/preprocessing/tables/termtopic/TermTopicFill.java
+++ plugin-hierarchicaltopic-preprocessing/src/main/java/cc/topicexplorer/plugin/hierarchicaltopic/preprocessing/tables/termtopic/TermTopicFill.java
@@ -1,143 +1,145 @@
 package cc.topicexplorer.plugin.hierarchicaltopic.preprocessing.tables.termtopic;
 
 /** MIT-JOOQ-START 
-import static jooq.generated.Tables.TOPIC;
-import static jooq.generated.Tables.TERM_TOPIC;
-MIT-JOOQ-ENDE */ 
+ import static jooq.generated.Tables.TOPIC;
+ import static jooq.generated.Tables.TERM_TOPIC;
+ MIT-JOOQ-ENDE */
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import cc.topicexplorer.chain.commands.TableFillCommand;
+
 /**
- * angefangen von Mattes weiterverarbeitet von Gert Kommaersetzung, Pfadangabe
- * eingefügt, Tabellenname mit Jooq verknüpft
+ * @author angefangen von Mattes weiterverarbeitet von Gert Kommaersetzung,
+ *         Pfadangabe eingefügt, Tabellenname mit Jooq verknüpft
  * 
  */
 public class TermTopicFill extends TableFillCommand {
 
-	public void fillTable() throws SQLException {
-		/** MIT-JOOQ-START 
+	@Override
+	public void fillTable() {
+		try {
+/** MIT-JOOQ-START 
 		ResultSet rsTopics = this.database.executeQuery("SELECT * FROM " 
 				+ TOPIC.getName() + " WHERE " 
 				+ TOPIC.HIERARCHICAL_TOPIC$START.getName() + "<" 
 				+ TOPIC.HIERARCHICAL_TOPIC$END.getName());
-		MIT-JOOQ-ENDE */ 	
-		/** OHNE_JOOQ-START */ 
-		ResultSet rsTopics = this.database.executeQuery("SELECT * FROM " 
-				+ "TOPIC" + " WHERE " 
-				+ "TOPIC.HIERARCHICAL_TOPIC$START" + "<" 
-				+ "TOPIC.HIERARCHICAL_TOPIC$END");
-		/** OHNE_JOOQ-ENDE */ 
-		rsTopics.last();
-		int rowCount = rsTopics.getRow();
-		
-		int topicIds[] = new int[rowCount];
-		int numTokens[] = new int[rowCount];
-		int clusterStart[] = new int[rowCount];
-		int clusterEnd[] = new int[rowCount];
-		
-		int i = 0;
-		rsTopics.beforeFirst();
-		/** MIT-JOOQ-START 
-		while(rsTopics.next()) {
-			topicIds[i] = rsTopics.getInt(TOPIC.TOPIC_ID.getName());
-			numTokens[i] = rsTopics.getInt(TOPIC.NUMBER_OF_TOKENS.getName());
-			clusterStart[i] = rsTopics.getInt(TOPIC.HIERARCHICAL_TOPIC$START.getName());
-			clusterEnd[i] = rsTopics.getInt(TOPIC.HIERARCHICAL_TOPIC$END.getName());
-			i++;
+		MIT-JOOQ-ENDE */
+			/** OHNE_JOOQ-START */
+//			@formatter:off
+			ResultSet rsTopics = this.database.executeQuery("SELECT * FROM " 
+					+ "TOPIC" + " WHERE " 
+					+ "TOPIC.HIERARCHICAL_TOPIC$START" + "<" 
+					+ "TOPIC.HIERARCHICAL_TOPIC$END");
+//			@formatter:on
+			/** OHNE_JOOQ-ENDE */
+			rsTopics.last();
+			int rowCount = rsTopics.getRow();
+
+			int topicIds[] = new int[rowCount];
+			int numTokens[] = new int[rowCount];
+			int clusterStart[] = new int[rowCount];
+			int clusterEnd[] = new int[rowCount];
+
+			int i = 0;
+			rsTopics.beforeFirst();
+			/**
+			 * MIT-JOOQ-START while(rsTopics.next()) { topicIds[i] =
+			 * rsTopics.getInt(TOPIC.TOPIC_ID.getName()); numTokens[i] =
+			 * rsTopics.getInt(TOPIC.NUMBER_OF_TOKENS.getName());
+			 * clusterStart[i] =
+			 * rsTopics.getInt(TOPIC.HIERARCHICAL_TOPIC$START.getName());
+			 * clusterEnd[i] =
+			 * rsTopics.getInt(TOPIC.HIERARCHICAL_TOPIC$END.getName()); i++; }
+			 * 
+			 * for(i = 0; i < rowCount; i++) {
+			 * database.executeUpdateQuery("INSERT INTO " + TERM_TOPIC.getName()
+			 * + " (" + TERM_TOPIC.TOPIC_ID.getName() + "," +
+			 * TERM_TOPIC.TERM_ID.getName() + "," +
+			 * TERM_TOPIC.NUMBER_OF_TOKEN_TOPIC.getName() + "," +
+			 * TERM_TOPIC.PR_TOPIC_GIVEN_TERM.getName() + "," +
+			 * TERM_TOPIC.PR_TERM_GIVEN_TOPIC.getName() + ") SELECT " +
+			 * topicIds[i] + "," + TERM_TOPIC.TERM_ID.getName() + "," + "SUM(" +
+			 * TERM_TOPIC.NUMBER_OF_TOKEN_TOPIC.getName() + ")," + "SUM(" +
+			 * TERM_TOPIC.PR_TOPIC_GIVEN_TERM.getName() + ")," + "SUM(" +
+			 * TERM_TOPIC.NUMBER_OF_TOKEN_TOPIC.getName() + ") / T." +
+			 * TOPIC.NUMBER_OF_TOKENS.getName() + " FROM " +
+			 * TERM_TOPIC.getName() + "," + TOPIC.getName() + " TB," +
+			 * TOPIC.getName() + " T WHERE " + TERM_TOPIC.getName() + "." +
+			 * TERM_TOPIC.TOPIC_ID.getName() + "=TB." + TOPIC.TOPIC_ID.getName()
+			 * + " AND " + "TB." + TOPIC.HIERARCHICAL_TOPIC$START.getName() +
+			 * "=" + "TB." + TOPIC.HIERARCHICAL_TOPIC$END.getName() + " AND " +
+			 * "TB." + TOPIC.HIERARCHICAL_TOPIC$START.getName() + ">=" +
+			 * clusterStart[i] + " AND " + "TB." +
+			 * TOPIC.HIERARCHICAL_TOPIC$END.getName() + "<=" + clusterEnd[i] +
+			 * " AND " + "T." + TOPIC.TOPIC_ID.getName() + "=" + topicIds[i] +
+			 * " GROUP BY " + TERM_TOPIC.TERM_ID.getName() + ", T." +
+			 * TOPIC.NUMBER_OF_TOKENS.getName());
+			 * 
+			 * } MIT-JOOQ-ENDE
+			 */
+			/** OHNE_JOOQ-START */
+			while (rsTopics.next()) {
+				topicIds[i] = rsTopics.getInt("TOPIC.TOPIC_ID");
+				numTokens[i] = rsTopics.getInt("TOPIC.NUMBER_OF_TOKENS");
+				clusterStart[i] = rsTopics.getInt("TOPIC.HIERARCHICAL_TOPIC$START");
+				clusterEnd[i] = rsTopics.getInt("TOPIC.HIERARCHICAL_TOPIC$END");
+				i++;
+			}
+
+			for (i = 0; i < rowCount; i++) {
+//				@formatter:off
+				database.executeUpdateQuery("INSERT INTO " 
+						+ "TERM_TOPIC" + " (" 
+						+ "TERM_TOPIC.TOPIC_ID" + ","
+						+ "TERM_TOPIC.TERM_ID" + ","
+						+ "TERM_TOPIC.NUMBER_OF_TOKEN_TOPIC" + ","
+						+ "TERM_TOPIC.PR_TOPIC_GIVEN_TERM" + "," 
+						+ "TERM_TOPIC.PR_TERM_GIVEN_TOPIC" + ") SELECT "
+						+ topicIds[i] + ","
+						+ "TERM_TOPIC.TERM_ID" + ","
+						+ "SUM(" + "TERM_TOPIC.NUMBER_OF_TOKEN_TOPIC" + ")," 
+						+ "SUM(" + "TERM_TOPIC.PR_TOPIC_GIVEN_TERM" + ")," 
+						+ "SUM(" + "TERM_TOPIC.NUMBER_OF_TOKEN_TOPIC" + ") / T." 
+						+ "NUMBER_OF_TOKENS"
+						+ " FROM "
+						+ "TERM_TOPIC" + "," 
+						+ "TOPIC" + " TB,"
+						+ "TOPIC" + " T WHERE "
+						+ "TERM_TOPIC.TOPIC_ID" + "=TB."
+						+ "TOPIC_ID" + " AND "
+						+ "TB." + "HIERARCHICAL_TOPIC$START" + "="
+						+ "TB." + "HIERARCHICAL_TOPIC$END" + " AND "
+						+ "TB." + "HIERARCHICAL_TOPIC$START" + ">="
+						+ clusterStart[i] + " AND "
+						+ "TB." + "HIERARCHICAL_TOPIC$END" + "<="
+						+ clusterEnd[i] + " AND "
+						+ "T." + "TOPIC_ID" + "="
+						+ topicIds[i]
+						+ " GROUP BY " + "TERM_TOPIC.TERM_ID"
+						+ ", T." + "NUMBER_OF_TOKENS");
+//				@formatter:on
+			}
+		} catch (SQLException e) {
+			logger.error("Table " + this.tableName + " could not be filled properly.");
+			throw new RuntimeException(e);
 		}
-		
-		for(i = 0; i < rowCount; i++) {
-			database.executeUpdateQuery("INSERT INTO " 
-					+ TERM_TOPIC.getName() + " (" 
-					+ TERM_TOPIC.TOPIC_ID.getName() + ","
-					+ TERM_TOPIC.TERM_ID.getName() + ","
-					+ TERM_TOPIC.NUMBER_OF_TOKEN_TOPIC.getName() + ","
-					+ TERM_TOPIC.PR_TOPIC_GIVEN_TERM.getName() + "," 
-					+ TERM_TOPIC.PR_TERM_GIVEN_TOPIC.getName() + ") SELECT "
-					+ topicIds[i] + ","
-					+ TERM_TOPIC.TERM_ID.getName() + ","
-					+ "SUM(" + TERM_TOPIC.NUMBER_OF_TOKEN_TOPIC.getName() + ")," 
-					+ "SUM(" + TERM_TOPIC.PR_TOPIC_GIVEN_TERM.getName() + ")," 
-					+ "SUM(" + TERM_TOPIC.NUMBER_OF_TOKEN_TOPIC.getName() + ") / T." 
-					+ TOPIC.NUMBER_OF_TOKENS.getName()
-					+ " FROM "
-					+ TERM_TOPIC.getName() + "," 
-					+ TOPIC.getName() + " TB,"
-					+ TOPIC.getName() + " T WHERE "
-					+ TERM_TOPIC.getName() + "." 
-					+ TERM_TOPIC.TOPIC_ID.getName() + "=TB."
-					+ TOPIC.TOPIC_ID.getName() + " AND "
-					+ "TB." + TOPIC.HIERARCHICAL_TOPIC$START.getName() + "="
-					+ "TB." + TOPIC.HIERARCHICAL_TOPIC$END.getName() + " AND "
-					+ "TB." + TOPIC.HIERARCHICAL_TOPIC$START.getName() + ">="
-					+ clusterStart[i] + " AND "
-					+ "TB." + TOPIC.HIERARCHICAL_TOPIC$END.getName() + "<="
-					+ clusterEnd[i] + " AND "
-					+ "T." + TOPIC.TOPIC_ID.getName() + "="
-					+ topicIds[i]
-					+ " GROUP BY " + TERM_TOPIC.TERM_ID.getName()
-					+ ", T." + TOPIC.NUMBER_OF_TOKENS.getName());
-			
-		}
-		MIT-JOOQ-ENDE */ 
-		/** OHNE_JOOQ-START */ 
-		while(rsTopics.next()) {
-			topicIds[i] = rsTopics.getInt("TOPIC.TOPIC_ID");
-			numTokens[i] = rsTopics.getInt("TOPIC.NUMBER_OF_TOKENS");
-			clusterStart[i] = rsTopics.getInt("TOPIC.HIERARCHICAL_TOPIC$START");
-			clusterEnd[i] = rsTopics.getInt("TOPIC.HIERARCHICAL_TOPIC$END");
-			i++;
-		}
-		
-		for(i = 0; i < rowCount; i++) {
-			database.executeUpdateQuery("INSERT INTO " 
-					+ "TERM_TOPIC" + " (" 
-					+ "TERM_TOPIC.TOPIC_ID" + ","
-					+ "TERM_TOPIC.TERM_ID" + ","
-					+ "TERM_TOPIC.NUMBER_OF_TOKEN_TOPIC" + ","
-					+ "TERM_TOPIC.PR_TOPIC_GIVEN_TERM" + "," 
-					+ "TERM_TOPIC.PR_TERM_GIVEN_TOPIC" + ") SELECT "
-					+ topicIds[i] + ","
-					+ "TERM_TOPIC.TERM_ID" + ","
-					+ "SUM(" + "TERM_TOPIC.NUMBER_OF_TOKEN_TOPIC" + ")," 
-					+ "SUM(" + "TERM_TOPIC.PR_TOPIC_GIVEN_TERM" + ")," 
-					+ "SUM(" + "TERM_TOPIC.NUMBER_OF_TOKEN_TOPIC" + ") / T." 
-					+ "NUMBER_OF_TOKENS"
-					+ " FROM "
-					+ "TERM_TOPIC" + "," 
-					+ "TOPIC" + " TB,"
-					+ "TOPIC" + " T WHERE "
-					+ "TERM_TOPIC.TOPIC_ID" + "=TB."
-					+ "TOPIC_ID" + " AND "
-					+ "TB." + "HIERARCHICAL_TOPIC$START" + "="
-					+ "TB." + "HIERARCHICAL_TOPIC$END" + " AND "
-					+ "TB." + "HIERARCHICAL_TOPIC$START" + ">="
-					+ clusterStart[i] + " AND "
-					+ "TB." + "HIERARCHICAL_TOPIC$END" + "<="
-					+ clusterEnd[i] + " AND "
-					+ "T." + "TOPIC_ID" + "="
-					+ topicIds[i]
-					+ " GROUP BY " + "TERM_TOPIC.TERM_ID"
-					+ ", T." + "NUMBER_OF_TOKENS");
-			
-		}
-		/** OHNE_JOOQ-ENDE */ 
-	}	
+		/** OHNE_JOOQ-ENDE */
+	}
 
 	@Override
 	public void setTableName() {
-	/** MIT-JOOQ-START 
-		this.tableName = TERM_TOPIC.getName();
-	MIT-JOOQ-ENDE */ 
-	/** OHNE_JOOQ-START */ 
+		/**
+		 * MIT-JOOQ-START this.tableName = TERM_TOPIC.getName(); MIT-JOOQ-ENDE
+		 */
+		/** OHNE_JOOQ-START */
 		this.tableName = "TERM";
-	/** OHNE_JOOQ-ENDE */ 
+		/** OHNE_JOOQ-ENDE */
 	}
-	
+
 	@Override
 	public void addDependencies() {
 		beforeDependencies.add("TermTopicFill");
 		beforeDependencies.add("HierarchicalTopic_TopicFill");
-	}	
+	}
 }
--- plugin-hierarchicaltopic-preprocessing/src/main/java/cc/topicexplorer/plugin/hierarchicaltopic/preprocessing/tables/topic/TopicCreate.java
+++ plugin-hierarchicaltopic-preprocessing/src/main/java/cc/topicexplorer/plugin/hierarchicaltopic/preprocessing/tables/topic/TopicCreate.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 
 import cc.topicexplorer.chain.commands.TableCreateCommand;
 
-/**
+/*
  * angefangen von Mattes weiterverarbeitet von Gert Kommaersetzung, Pfadangabe
  * eingefügt, Tabellenname mit Jooq verknüpft
  * 
@@ -12,34 +12,36 @@ import cc.topicexplorer.chain.commands.TableCreateCommand;
 public class TopicCreate extends TableCreateCommand {
 
 	@Override
-	public void createTable() throws SQLException {
-		this.database
-				.executeUpdateQuery("ALTER TABLE "
-						+ this.tableName
-						+ " ADD COLUMN HIERARCHICAL_TOPIC$START INT(11) , "
-						+ " ADD COLUMN HIERARCHICAL_TOPIC$END INT(11) , "
-						+ " ADD COLUMN HIERARCHICAL_TOPIC$DEPTH INT(11) , "
-						+ " ADD COLUMN HIERARCHICAL_TOPIC$CLUSTER_MEMBERSHIP TEXT COLLATE utf8_bin");
+	public void createTable() {
+		try {
+			this.database.executeUpdateQuery("ALTER TABLE " + this.tableName
+					+ " ADD COLUMN HIERARCHICAL_TOPIC$START INT(11) , "
+					+ " ADD COLUMN HIERARCHICAL_TOPIC$END INT(11) , "
+					+ " ADD COLUMN HIERARCHICAL_TOPIC$DEPTH INT(11) , "
+					+ " ADD COLUMN HIERARCHICAL_TOPIC$CLUSTER_MEMBERSHIP TEXT COLLATE utf8_bin");
+		} catch (SQLException e) {
+			logger.error("Columns HIERARCHICAL_TOPIC$START, $END, $DEPTH, $CLUSTER_MEMBERSHIP could not be added to table "
+					+ this.tableName);
+			throw new RuntimeException(e);
+		}
 	}
 
-	private void dropColumns() throws SQLException {
-
+	private void dropColumns() {
 		try {
-			this.database
-					.executeUpdateQuery("ALTER TABLE "
-							+ this.tableName
-							+ " DROP COLUMN HIERARCHICAL_TOPIC$START"
-							+ ", DROP COLUMN HIERARCHICAL_TOPIC$END"
-							+ ", DROP COLUMN HIERARCHICAL_TOPIC$DEPTH"
-							+ ", DROP COLUMN HIERARCHICAL_TOPIC$CLUSTER_MEMBERSHIP");
-		} catch (Exception e) {
-			logger.warn("TopicMetaData.dropColumns: Cannot drop column, perhaps it doesn't exists. Doesn't matter ;)");
-
+			this.database.executeUpdateQuery("ALTER TABLE " + this.tableName + " DROP COLUMN HIERARCHICAL_TOPIC$START"
+					+ ", DROP COLUMN HIERARCHICAL_TOPIC$END" + ", DROP COLUMN HIERARCHICAL_TOPIC$DEPTH"
+					+ ", DROP COLUMN HIERARCHICAL_TOPIC$CLUSTER_MEMBERSHIP");
+		} catch (SQLException e) {
+			if (e.getErrorCode() != 1091) { // MySQL Error code for 'Can't DROP
+				// ..; check that column/key exists
+				logger.error("TopicMetaData.dropColumns: Cannot drop column.");
+				throw new RuntimeException(e);
+			}
 		}
 	}
 
 	@Override
-	public void dropTable() throws SQLException {
+	public void dropTable() {
 		this.dropColumns();
 	}
 
@@ -47,9 +49,9 @@ public class TopicCreate extends TableCreateCommand {
 	public void setTableName() {
 		this.tableName = "TOPIC";
 	}
-	
+
 	@Override
 	public void addDependencies() {
 		beforeDependencies.add("TopicCreate");
-	}	
+	}
 }

215b122cd69141fb57b48c12616dd0d9a29b22c7
Benjamin Schandera <4.32@gmx.de>
2014-01-22 20:52:09 +0000

    FEHLERB. (link-preprocessing): - Kommandos entscheiden selbständig, ob sie erfolgreich abgearbeitet wurden - Checked Exceptions aus Methodensignatur entfernen - kleinere Überarbeitungen

--- plugin-link-preprocessing/src/main/java/cc/topicexplorer/plugin/link/preprocessing/tables/document/DocumentCreate.java
+++ plugin-link-preprocessing/src/main/java/cc/topicexplorer/plugin/link/preprocessing/tables/document/DocumentCreate.java
@@ -11,24 +11,29 @@ import cc.topicexplorer.chain.commands.TableCreateCommand;
 public class DocumentCreate extends TableCreateCommand {
 
 	@Override
-	public void createTable() throws SQLException {
-		database.executeUpdateQuery("ALTER IGNORE TABLE `"
-					+ this.tableName
+	public void createTable() {
+		try {
+			database.executeUpdateQuery("ALTER IGNORE TABLE `" + this.tableName
 					+ "` ADD COLUMN LINK$URL VARCHAR(500) COLLATE UTF8_BIN,"
 					+ " ADD COLUMN LINK$IN_DEGREE INT(11) DEFAULT 1;");
+		} catch (SQLException e) {
+			logger.error("Columns LINK$URL, LINK$IN_DEGREE could not be added to table " + this.tableName);
+			throw new RuntimeException(e);
+		}
 	}
 
 	@Override
-	public void dropTable() throws SQLException {
+	public void dropTable() {
 		try {
-			this.database
-					.executeUpdateQuery("ALTER TABLE "
-							+ this.tableName
-							+ " DROP COLUMN LINK$URL"
-							+ ", DROP COLUMN LINK$IN_DEGREE");
-		} catch (Exception e) {
-			logger.warn("Document.dropColumns: Cannot drop column, perhaps it doesn't exists. Doesn't matter ;)");
-
+			this.database.executeUpdateQuery("ALTER TABLE " + this.tableName + " DROP COLUMN LINK$URL"
+					+ ", DROP COLUMN LINK$IN_DEGREE");
+		} catch (SQLException e) {
+			if (e.getErrorCode() != 1091) { // MySQL Error code for: 'Can't
+											// DROP..; check that column/key
+											// exists
+				logger.error("Document.dropColumns: Cannot drop column.");
+				throw new RuntimeException(e);
+			}
 		}
 	}
 
@@ -36,9 +41,9 @@ public class DocumentCreate extends TableCreateCommand {
 	public void setTableName() {
 		tableName = "DOCUMENT";
 	}
-	
+
 	@Override
 	public void addDependencies() {
 		beforeDependencies.add("DocumentCreate");
-	}	
+	}
 }
--- plugin-link-preprocessing/src/main/java/cc/topicexplorer/plugin/link/preprocessing/tables/document/DocumentFill.java
+++ plugin-link-preprocessing/src/main/java/cc/topicexplorer/plugin/link/preprocessing/tables/document/DocumentFill.java
@@ -3,60 +3,59 @@ package cc.topicexplorer.plugin.link.preprocessing.tables.document;
 import java.sql.SQLException;
 
 import cc.topicexplorer.chain.commands.TableFillCommand;
-/** MIT-JOOQ-START
-import static jooq.generated.Tables.DOCUMENT;
-MIT-JOOQ-ENDE */ 
 
 /**
- * @author user
- * 
+ * MIT-JOOQ-START import static jooq.generated.Tables.DOCUMENT; MIT-JOOQ-ENDE
  */
 public class DocumentFill extends TableFillCommand {
 
 	@Override
-	public void fillTable() throws SQLException {
-		
-		if (Boolean.parseBoolean(properties.getProperty("plugin_link"))) {
-			/** MIT-JOOQ-START
-			database.executeUpdateQuery("UPDATE " + this.tableName + " d, "
-					+ " (SELECT " + properties.getProperty("Link_OrgTableIndegree")
-					+ " AS indeg, " + properties.getProperty("Link_OrgTableLink")
-					+ " AS link, " + properties.getProperty("OrgTableId")
-					+ " AS id FROM " + properties.getProperty("OrgTableName")
-					+ " ) AS org SET "
-					+ DOCUMENT.LINK$URL.getName() + " = org.link, "
-					+ DOCUMENT.LINK$IN_DEGREE.getName() + " = org.indeg"
-					+ " WHERE org.id = d." + DOCUMENT.DOCUMENT_ID.getName());
-			MIT-JOOQ-ENDE */ 
-			/** OHNE_JOOQ-START */ 
+	public void fillTable() {
 
-			database.executeUpdateQuery("UPDATE " + this.tableName + " d, "
-					+ " (SELECT " + properties.getProperty("Link_OrgTableIndegree")
-					+ " AS indeg, " + properties.getProperty("Link_OrgTableLink")
-					+ " AS link, " + properties.getProperty("OrgTableId")
-					+ " AS id FROM " + properties.getProperty("OrgTableName")
-					+ " ) AS org SET "
-					+ "d.LINK$URL" + " = org.link, "
-					+ "d.LINK$IN_DEGREE" + " = org.indeg"
-					+ " WHERE org.id = d." + "DOCUMENT_ID");
-			/** OHNE_JOOQ-ENDE */ 
+		if (Boolean.parseBoolean(properties.getProperty("plugin_link"))) {
+			/**
+			 * MIT-JOOQ-START database.executeUpdateQuery("UPDATE " +
+			 * this.tableName + " d, " + " (SELECT " +
+			 * properties.getProperty("Link_OrgTableIndegree") + " AS indeg, " +
+			 * properties.getProperty("Link_OrgTableLink") + " AS link, " +
+			 * properties.getProperty("OrgTableId") + " AS id FROM " +
+			 * properties.getProperty("OrgTableName") + " ) AS org SET " +
+			 * DOCUMENT.LINK$URL.getName() + " = org.link, " +
+			 * DOCUMENT.LINK$IN_DEGREE.getName() + " = org.indeg" +
+			 * " WHERE org.id = d." + DOCUMENT.DOCUMENT_ID.getName());
+			 * MIT-JOOQ-ENDE
+			 */
+			/** OHNE_JOOQ-START */
+
+			try {
+				database.executeUpdateQuery("UPDATE " + this.tableName + " d, " + " (SELECT "
+						+ properties.getProperty("Link_OrgTableIndegree") + " AS indeg, "
+						+ properties.getProperty("Link_OrgTableLink") + " AS link, "
+						+ properties.getProperty("OrgTableId") + " AS id FROM "
+						+ properties.getProperty("OrgTableName") + " ) AS org SET " + "d.LINK$URL" + " = org.link, "
+						+ "d.LINK$IN_DEGREE" + " = org.indeg" + " WHERE org.id = d." + "DOCUMENT_ID");
+			} catch (SQLException e) {
+				logger.error("Table " + this.tableName + " could not be filled properly.");
+				throw new RuntimeException(e);
+			}
+			/** OHNE_JOOQ-ENDE */
 		}
 
 	}
 
 	@Override
 	public void setTableName() {
-	/** MIT-JOOQ-START
-		tableName = DOCUMENT.getName();
-		MIT-JOOQ-ENDE */ 
-	/** OHNE_JOOQ-START */ 	
+		/**
+		 * MIT-JOOQ-START tableName = DOCUMENT.getName(); MIT-JOOQ-ENDE
+		 */
+		/** OHNE_JOOQ-START */
 		this.tableName = "DOCUMENT";
-	/** OHNE_JOOQ-ENDE */ 
+		/** OHNE_JOOQ-ENDE */
 	}
-	
+
 	@Override
 	public void addDependencies() {
 		beforeDependencies.add("DocumentFill");
 		beforeDependencies.add("Link_DocumentCreate");
-	}	
+	}
 }

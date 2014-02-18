b5b078a1375d33af7111376ba3929247b16780bf
Gert Boehmer <gertb@gmx.de>
2014-02-18 14:26:47 +0000

    drop table incl exception eingefügt

--- plugin-wiki-preprocessing/src/main/java/cc/topicexplorer/plugin/wiki/preprocessing/tables/categorytree/CategoryTreeCreate.java
+++ plugin-wiki-preprocessing/src/main/java/cc/topicexplorer/plugin/wiki/preprocessing/tables/categorytree/CategoryTreeCreate.java
@@ -8,6 +8,20 @@ public class CategoryTreeCreate extends TableCreateCommand {
 
 	@Override
 	public void createTable() {
+
+		// drop table
+		try {
+			this.database.executeUpdateQuery("drop table " + this.tableName + ";");
+		} catch (SQLException e) {
+			if (e.getErrorCode() != 1051) { // MySQL Error code for 'Can't
+				// DROP
+				// ..; check that table exists
+				logger.error("wiki-plug-in." + this.tableName + ": Cannot drop table " + this.tableName);
+				throw new RuntimeException(e);
+			}
+		}
+
+		// create table
 		try {
 			database.executeUpdateQuery("create table `" + this.tableName + "` (" + "CAT$CHILD VARCHAR(255) NOT NULL, "
 					+ "CAT$PARENT VARCHAR (255) not null ) " + "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;");

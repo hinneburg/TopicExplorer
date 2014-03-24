7d4f307af87884295df495d13dd064999f3d30ee
Mattes Angelus <mattes.angelus@japanologie.uni-halle.de>
2014-03-24 11:28:05 +0100

    #2 eliminated localhost from ajax loading

--- webapp/src/main/webapp/scripts/modules/topicexplorer-model.js
+++ webapp/src/main/webapp/scripts/modules/topicexplorer-model.js
@@ -2,14 +2,14 @@ define(["jquery"], function($) {
 	self = this;
 	self.topicexplorerModel = new Object();
 	self.topicexplorerModel.loadDocuments = function (parameter, callback) {
-		$.getJSON("http://localhost:8080/webapp/JsonServlet?" + parameter.paramString).success(function(receivedParsedJson) {
+		$.getJSON("JsonServlet?" + parameter.paramString).success(function(receivedParsedJson) {
 			$.extend(self.topicexplorerModel.document, receivedParsedJson.DOCUMENT);
 			callback(receivedParsedJson.DOCUMENT_SORTING);
 			self.topicexplorerModel.documentsLoading(false);
 		});
 	};
 	self.topicexplorerModel.loadDocument = function (parameter, callback) {
-		$.getJSON("http://localhost:8080/webapp/JsonServlet?" + parameter.paramString).success(function(receivedParsedJson) {
+		$.getJSON("JsonServlet?" + parameter.paramString).success(function(receivedParsedJson) {
 			$.extend(self.topicexplorerModel.document[receivedParsedJson.DOCUMENT.DOCUMENT_ID], receivedParsedJson.DOCUMENT);
 			callback(receivedParsedJson.DOCUMENT.DOCUMENT_ID);
 			self.topicexplorerModel.documentsLoading(false);

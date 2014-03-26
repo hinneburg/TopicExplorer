034474242cd890855f1df3687e83fc75a0bc2371
Mattes Angelus <mattes.angelus@japanologie.uni-halle.de>
2014-03-26 11:28:16 +0100

    #5 text plugin is default plugin template

--- webapp/src/main/webapp/scripts/modules/topicexplorer-view-config.js
+++ webapp/src/main/webapp/scripts/modules/topicexplorer-view-config.js
@@ -1,19 +1,19 @@
 define(["scripts/modules/topicexplorer-model"], function(topicexplorerModel) {
     topicexplorerModel.config = {	
 			documentView:	{
-				activePlugin: 0,
+				activePlugin: 1,
 				pluginTemplates: [ "document-view-id", 
 					               "document-view-plugin-text"					
 								 ]   
 			},
 			singleView:	{
-				activePlugin: 0,
+				activePlugin: 1,
 				pluginTemplates: [ "single-view-id", 
 					               "single-view-plugin-text"					
 								 ]   
 			},
 			topicView:	{
-				activePlugin: 0,
+				activePlugin: 1,
 				pluginTemplates: [ "topic-view-id", 
 					               "topic-view-plugin-text", 
 								   "topic-view-plugin-frame"

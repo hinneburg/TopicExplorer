3125c7e4e7502af9d22a32b4d69c332af64e09a5
husterknupp <4.23@gmx.de>
2014-04-11 21:42:35 +0200

    eclipse autoformat (wrap line)

--- webapp/src/main/java/cc/topicexplorer/web/OnStartup.java
+++ webapp/src/main/java/cc/topicexplorer/web/OnStartup.java
@@ -41,16 +41,17 @@ public class OnStartup implements ServletContextListener {
 
 	@Override
 	public void contextDestroyed(ServletContextEvent arg0) {
-		 // This manually deregisters JDBC driver, which prevents Tomcat 7 from complaining about memory leaks wrto this class 
-		Enumeration<Driver> drivers = DriverManager.getDrivers(); 
-		while (drivers.hasMoreElements()) { 
-			Driver driver = drivers.nextElement(); 
-			try { 
-				DriverManager.deregisterDriver(driver); 
-				logger.log(Level.INFO, String.format("deregistering jdbc driver: %s", driver)); 
-			} catch (SQLException e) { 
-				logger.log(Level.ERROR, String.format("Error deregistering driver %s", driver), e); 
-			} 
+		// This manually deregisters JDBC driver, which prevents Tomcat 7 from
+		// complaining about memory leaks wrto this class
+		Enumeration<Driver> drivers = DriverManager.getDrivers();
+		while (drivers.hasMoreElements()) {
+			Driver driver = drivers.nextElement();
+			try {
+				DriverManager.deregisterDriver(driver);
+				logger.log(Level.INFO, String.format("deregistering jdbc driver: %s", driver));
+			} catch (SQLException e) {
+				logger.log(Level.ERROR, String.format("Error deregistering driver %s", driver), e);
+			}
 		}
 	}
 

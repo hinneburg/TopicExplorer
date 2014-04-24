27ba95cc575988f5f9e07d042ece52a3e63ce0e5
Mattes Angelus <mattes.angelus@japanologie.uni-halle.de>
2014-04-24 16:42:32 +0200

    #9 multichart prototype

--- webapp/src/main/webapp/scripts/modules/time-view.js
+++ webapp/src/main/webapp/scripts/modules/time-view.js
@@ -30,31 +30,55 @@ define(
 		self.activeTab.subscribe(function(newValue) {
 			if(topicexplorerModel.view.tab[newValue].module == "time-view") {
 				self.topicId(topicexplorerModel.view.tab[newValue].focus);
-				
+				if(typeof topicexplorerModel.view.tab[topicexplorerModel.view.activeTab].renderedTopics == 'undefined')
+	    			topicexplorerModel.view.tab[topicexplorerModel.view.activeTab].renderedTopics = new Array("" + self.topicId());
+	    		self.renderedTopics(topicexplorerModel.view.tab[topicexplorerModel.view.activeTab].renderedTopics);
+	    		
 				self.makeChart();
 			}
 		});
 		
+		
     	self.topicId = ko.observable(topicexplorerModel.view.tab[topicexplorerModel.view.activeTab].focus);
+    	
+    	self.renderedTopics = ko.observableArray();
+		self.renderedTopics.subscribe(function(newValue) {
+			if(topicexplorerModel.view.tab[topicexplorerModel.view.activeTab].renderedTopics.length > newValue.length) {
+				var diff = $(topicexplorerModel.view.tab[topicexplorerModel.view.activeTab].renderedTopics).not(newValue).get();
+				self.chart.get('topicChart' + diff[0]).remove();
+			} else if(topicexplorerModel.view.tab[topicexplorerModel.view.activeTab].renderedTopics.length < newValue.length) {
+				var diff = $(newValue).not(topicexplorerModel.view.tab[topicexplorerModel.view.activeTab].renderedTopics).get();
+				self.addChart(diff[0]);
+			}
+			topicexplorerModel.view.tab[topicexplorerModel.view.activeTab].renderedTopics = newValue.slice();
+		});
+    	
     	self.makeChart = function() {
     		self.topicId(topicexplorerModel.view.tab[topicexplorerModel.view.activeTab].focus);
     		self.chart = new Highcharts.StockChart({
 		   	    chart: {
 		    	   renderTo: 'chart',
 		    	   height: self.timeDesktopHeight()
-		    	},
-		    	series: [{
-		    	   data: topicexplorerModel.data.topic[self.topicId()].TIME$WORDS_PER_WEEK,
-		    	   color: topicexplorerModel.data.topic[self.topicId()].COLOR_TOPIC$COLOR
-		    	}]
+		    	}
 		    });
+    		for(var i = 0; i < topicexplorerModel.view.tab[topicexplorerModel.view.activeTab].renderedTopics.length; i++ ) {
+    			self.addChart(topicexplorerModel.view.tab[topicexplorerModel.view.activeTab].renderedTopics[i]);
+    		}
+    	};
+    	
+    	self.addChart = function(topicId) {
+    		self.chart.addSeries({
+    			id: 'topicChart' + topicId,
+    			data: topicexplorerModel.data.topic[topicId].TIME$WORDS_PER_WEEK,
+		    	color: topicexplorerModel.data.topic[topicId].COLOR_TOPIC$COLOR
+    		});
     	};
     	
     	self.init = function() {
     		if(typeof topicexplorerModel.data.timeDataLoaded == 'undefined') {
-				
+    			topicexplorerModel.view.tab[topicexplorerModel.view.activeTab].renderedTopics = new Array("" + self.topicId());
+	    		self.renderedTopics(topicexplorerModel.view.tab[topicexplorerModel.view.activeTab].renderedTopics);
 				$.getJSON("JsonServlet?Command=getDates").success(function(receivedParsedJson) {
-				//	timeData = topicexplorerModel.convertWeek2Date(receivedParsedJson);
 					for (key in receivedParsedJson) {
 					    $.extend(self.topicexplorerModel.data.topic[key], receivedParsedJson[key]);
 					}

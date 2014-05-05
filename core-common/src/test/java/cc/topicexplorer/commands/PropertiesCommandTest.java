f79595ba0c3d38120c94ce45ce2d5ff63857c15a
husterknupp <4.23@gmx.de>
2014-05-05 11:02:27 +0200

    rework commit 5bd3808

--- core-common/src/test/java/cc/topicexplorer/commands/PropertiesCommandTest.java
+++ core-common/src/test/java/cc/topicexplorer/commands/PropertiesCommandTest.java
@@ -4,6 +4,7 @@ import static org.fest.assertions.Assertions.assertThat;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 import org.junit.Test;
@@ -12,29 +13,18 @@ import cc.commandmanager.core.CommunicationContext;
 
 public class PropertiesCommandTest {
 
-	/*
-	 * mock context
-	 */
-	/*
-	 * lass ein Mal das Command ausführen und speichere die dann enthaltenen
-	 * properties in "Rohform" im Code dieses Tests
-	 */
-	/*
-	 * Alle Änderungen müssen dann wieder diese Properties erstellen
-	 */
-
-	PropertiesCommand objectUnderTest = new PropertiesCommand();
+	PropertiesCommand commandUnderTest = new PropertiesCommand();
 
 	@Test
 	public void testExecute() throws IOException {
+		byte[] targetPropertiesRaw = "plugin_hierarchicaltopic=true\nOrgTableId=id\nBitDepth=32\ndatabase.NumberOfRetries=3\nplugin_wordtype=true\nDocBrowserLimit=20\nmalletNumTopics=10\nOrgTableTxt=txt\ndatabase.DbUser=root\nplugins=text,hierarchicaltopic,colortopic,wordtype,fulltext\ndatabase.DbLocation=localhost:3306\ndatabase.DbPassword=TopicExplorer\nplugin_text=true\nFrontendViews=slider,topic,text\nRAM=1g\nFulltext_OrgTableFulltext=txt\nplugin_fulltext=true\nplugin_colortopic=true\ndatabase.DB=japantest\nInCSVFile=/home/user/japantest/db.csv\nOrgTableName=db"
+				.getBytes();
+		InputStream propertiesStream = new ByteArrayInputStream(targetPropertiesRaw);
 		Properties targetProperties = new Properties();
-		targetProperties
-				.load(new ByteArrayInputStream(
-						"plugin_hierarchicaltopic=true\nOrgTableId=id\nBitDepth=32\ndatabase.NumberOfRetries=3\nplugin_wordtype=true\nDocBrowserLimit=20\nmalletNumTopics=10\nOrgTableTxt=txt\ndatabase.DbUser=root\nplugins=text,hierarchicaltopic,colortopic,wordtype,fulltext\ndatabase.DbLocation=localhost:3306\ndatabase.DbPassword=TopicExplorer\nplugin_text=true\nFrontendViews=slider,topic,text\nRAM=1g\nFulltext_OrgTableFulltext=txt\nplugin_fulltext=true\nplugin_colortopic=true\ndatabase.DB=japantest\nInCSVFile=/home/user/japantest/db.csv\nOrgTableName=db"
-								.getBytes()));
+		targetProperties.load(propertiesStream);
 
 		CommunicationContext communicationContext = new CommunicationContext();
-		objectUnderTest.execute(communicationContext);
+		commandUnderTest.execute(communicationContext);
 
 		Properties actualProperties = (Properties) communicationContext.get(PropertiesCommand.PROPERTIES_KEY);
 		assertThat(actualProperties).isEqualTo(targetProperties);

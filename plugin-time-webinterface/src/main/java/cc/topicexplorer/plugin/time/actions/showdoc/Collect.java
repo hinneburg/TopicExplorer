b11a8ee0ef5c0ea9537a7c05120b06ecb2fb540e
Mattes Angelus <mattes.angelus@japanologie.uni-halle.de>
2014-07-02 13:23:02 +0200

    #74 showdoc web action extended for getting date

---
+++ plugin-time-webinterface/src/main/java/cc/topicexplorer/plugin/time/actions/showdoc/Collect.java
@@ -0,0 +1,42 @@
+package cc.topicexplorer.plugin.time.actions.showdoc;
+
+import java.util.Set;
+
+import cc.commandmanager.core.Context;
+import cc.topicexplorer.commands.TableSelectCommand;
+import cc.topicexplorer.database.SelectMap;
+
+import com.google.common.collect.Sets;
+
+public class Collect extends TableSelectCommand {
+
+	@Override
+	public void tableExecute(Context context) {
+		SelectMap documentMap = context.get("DOCUMENT_QUERY", SelectMap.class);
+
+		documentMap.select.add("DOCUMENT.TIME$TIME_STAMP");
+
+		context.rebind("DOCUMENT_QUERY", documentMap);
+	}
+
+	@Override
+	public Set<String> getAfterDependencies() {
+		return Sets.newHashSet("ShowDocCoreGenerateSQL");
+	}
+
+	@Override
+	public Set<String> getBeforeDependencies() {
+		return Sets.newHashSet("ShowDocCoreCreate");
+	}
+
+	@Override
+	public Set<String> getOptionalAfterDependencies() {
+		return Sets.newHashSet();
+	}
+
+	@Override
+	public Set<String> getOptionalBeforeDependencies() {
+		return Sets.newHashSet();
+	}
+
+}

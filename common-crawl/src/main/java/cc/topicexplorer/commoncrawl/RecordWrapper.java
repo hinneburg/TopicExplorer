5c0026fe6cbe4223d132d19c4b3be26d076dd842
Florian Lücke <florian.luecke2@gmail.com>
2014-08-20 09:03:47 +0200

    adds a wrapper for ArchiveRecord
    the contents of the record can only be read once, this class makes
    reading multiple times possible

---
+++ common-crawl/src/main/java/cc/topicexplorer/commoncrawl/RecordWrapper.java
@@ -0,0 +1,84 @@
+package cc.topicexplorer.commoncrawl;
+
+import java.io.IOException;
+
+import org.apache.commons.io.IOUtils;
+import org.archive.io.ArchiveRecord;
+import org.archive.io.ArchiveRecordHeader;
+
+public class RecordWrapper {
+    private static final String HTTP_HEADER_START     = "HTTP/";
+    private static final int    HTTP_HEADER_START_LEN = HTTP_HEADER_START.length();
+    private static final String HTTP_HEADER_END       = "\r\n\r\n";
+    private String              content               = null;
+    private ArchiveRecordHeader header                = null;
+
+    public RecordWrapper(ArchiveRecord rec) {
+        this.content = extractRecordBody(rec);
+        this.header = rec.getHeader();
+    }
+
+    /**
+     * Extracts the body from an ArchiveRecord.
+     * The body contains the full HTPP response of the server.
+     * 
+     * @param record
+     *            the record to extract from
+     * @return the requested body
+     */
+    protected static String extractRecordBody(ArchiveRecord record) {
+        try {
+            byte[] rawData = IOUtils.toByteArray(record, record.available());
+            String content = new String(rawData);
+            return content;
+        } catch (IOException e) {
+            return "";
+        }
+    }
+
+    /**
+     * Get the content of the record.
+     * 
+     * @return the requested content
+     */
+    public String getContent() {
+        return content;
+    }
+
+    public ArchiveRecordHeader getHeader() {
+        return this.header;
+    }
+
+    /**
+     * Get only the HTTP body.
+     * 
+     * @return the requested body
+     */
+    public String getHTTPBody() {
+        String content = this.getContent();
+
+        if (content.substring(0, HTTP_HEADER_START_LEN).equals(HTTP_HEADER_START)) {
+            String body = content.substring(content.indexOf("\r\n\r\n") + 4);
+            return body;
+        }
+        return content;
+    }
+
+    /**
+     * Get only the HTTP header.
+     * 
+     * @return the requested header
+     */
+    public String getHTTPHeader() {
+        String content = this.getContent();
+        int headerStart = content.indexOf(HTTP_HEADER_START);
+
+        if (headerStart == -1) {
+            return "";
+        }
+
+        int headerEnd = content.indexOf(HTTP_HEADER_END, headerStart);
+        String header = content.substring(headerStart, headerEnd);
+        return header;
+    }
+}
---
+++ common-crawl/src/test/java/cc/topicexplorer/commoncrawl/RecordWrapperTest.java
@@ -0,0 +1,74 @@
+package cc.topicexplorer.commoncrawl;
+
+import static cc.topicexplorer.commoncrawl.RecordWrapper.extractRecordBody;
+import static org.junit.Assert.assertEquals;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.archive.io.ArchiveRecord;
+import org.archive.io.warc.WARCRecord;
+import org.junit.Test;
+
+public class RecordWrapperTest {
+    private static final String TEST_RECORD_NAME = "/TestRecord.warc";
+    private static final String TEST_RECORD_PATH = RecordWrapperTest.class.getResource(TEST_RECORD_NAME).getPath();
+
+    private static ArchiveRecord makeNewRecord() throws FileNotFoundException,
+        IOException {
+        FileInputStream f = new FileInputStream(TEST_RECORD_PATH);
+        ArchiveRecord rec = new WARCRecord(f, "Test Record", 0);
+        return rec;
+    }
+
+    @Test
+    public void testExtractRecordBody() throws FileNotFoundException,
+        IOException {
+        ArchiveRecord record = makeNewRecord();
+        assertEquals(extractRecordBody(record),
+                     "HTTP/1.1 200 OK\r\n"
+                             + "Server: nginx/1.6.0\r\n"
+                             + "Date: Wed, 09 Jul 2014 23:30:51 GMT\r\n"
+                             + "Content-Type: text/plain; charset=UTF-8\r\n"
+                             + "\r\n" + "12345");
+    }
+
+    @Test
+    public void testGetHeader() throws FileNotFoundException, IOException {
+        ArchiveRecord record = makeNewRecord();
+        RecordWrapper w = new RecordWrapper(record);
+        assertEquals(record.getHeader(), w.getHeader());
+        assertEquals(record.getHeader(), w.getHeader());
+    }
+
+    @Test
+    public void testGetHTTPBody() throws FileNotFoundException, IOException {
+        ArchiveRecord record = makeNewRecord();
+        RecordWrapper w = new RecordWrapper(record);
+        assertEquals(w.getHTTPBody(), "12345");
+
+        // the reason the wrapper exists is so that we can
+        // get the contents multiple times. so try at least twice
+        assertEquals(w.getHTTPBody(), "12345");
+    }
+
+    @Test
+    public void testGetHTTPHeader() throws FileNotFoundException, IOException {
+        ArchiveRecord record = makeNewRecord();
+        RecordWrapper w = new RecordWrapper(record);
+        assertEquals(w.getHTTPHeader(),
+                     "HTTP/1.1 200 OK\r\n"
+                             + "Server: nginx/1.6.0\r\n"
+                             + "Date: Wed, 09 Jul 2014 23:30:51 GMT\r\n"
+                             + "Content-Type: text/plain; charset=UTF-8");
+
+        // the reason the wrapper exists is so that we can
+        // get the contents multiple times. so try at least twice
+        assertEquals(w.getHTTPHeader(),
+                     "HTTP/1.1 200 OK\r\n"
+                             + "Server: nginx/1.6.0\r\n"
+                             + "Date: Wed, 09 Jul 2014 23:30:51 GMT\r\n"
+                             + "Content-Type: text/plain; charset=UTF-8");
+    }
+}

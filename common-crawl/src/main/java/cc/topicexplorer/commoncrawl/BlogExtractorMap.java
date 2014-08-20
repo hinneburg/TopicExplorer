dc1f45ad3787602afc34c2310370a13bc653e84a
Florian Lücke <florian.luecke2@gmail.com>
2014-08-20 08:58:45 +0200

    reformats code in BlogExtractorMap

--- common-crawl/src/main/java/cc/topicexplorer/commoncrawl/BlogExtractorMap.java
+++ common-crawl/src/main/java/cc/topicexplorer/commoncrawl/BlogExtractorMap.java
@@ -4,17 +4,21 @@ import java.io.IOException;
 
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.log4j.Logger;
 import org.archive.io.ArchiveReader;
 
 public class BlogExtractorMap {
-    /**
+    private static final Logger LOG = Logger.getLogger(BlogExtractorMap.class);
+
+    /*
      * Implements the map function for MapReduce.
      */
-    public static class BlogExtractorMapper extends Mapper<Text, ArchiveReader, Text, Text> {
-
+    public static class BlogExtractorMapper extends
+        Mapper<Text, ArchiveReader, Text, Text> {
         // implement the main "map" function
         @Override
-        public void map(Text key, ArchiveReader value, Context context) throws IOException, InterruptedException {
+        public void map(Text key, ArchiveReader value, Context context)
+            throws IOException, InterruptedException {
         }
     }
 }

95c7265cbed35cbf10a0c762cbdc890b29e1f7bf
Florian Lücke <florian.luecke2@gmail.com>
2014-08-22 14:28:08 +0200

    removes unneeded import

--- common-crawl/src/main/java/cc/topicexplorer/commoncrawl/BlogExtractorTool.java
+++ common-crawl/src/main/java/cc/topicexplorer/commoncrawl/BlogExtractorTool.java
@@ -12,7 +12,6 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;

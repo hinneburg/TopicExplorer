2a5392ac4132e0cbd1df67201dbd492c27d46889
Slayer <slayer@ubuntu.ubuntu-domain>
2014-08-29 16:47:19 +0200

    #74 POS dispatcher created

---
+++ plugin-pos-preprocessing/src/main/java/cc/topicexplorer/plugin/pos/preprocessing/dispatchers/Dispatcher.java
@@ -0,0 +1,37 @@
+package cc.topicexplorer.plugin.pos.preprocessing.dispatchers;
+
+import java.util.List;
+
+public class Dispatcher {
+	
+	public static final int german = 1;
+	public static final int english = 2;
+	public static final int japaneese = 3;
+	
+	int language = -1;
+	
+	public static Dispatcher getDispatcher(int language)
+	{
+		switch (language)
+		{
+		case german: 
+		{
+			return new nlpDispatcher(language);
+		}
+		case english:
+		{
+			return new nlpDispatcher(language);
+		}
+		} 
+		return null;
+	}
+	
+	public Dispatcher(int language)
+	{
+		this.language = language;
+	}
+	
+	public void initialize(String path){}
+	public int getSentencesNumber() {return 0;}
+	public List<String[]> tokenizeSentence(int sentenceNumber) { return null;}
+}

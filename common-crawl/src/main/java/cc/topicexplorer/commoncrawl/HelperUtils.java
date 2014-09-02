9e83b611c8f1608bd39eb37994514a1a7d12cac1
Florian Lücke <florian.luecke2@gmail.com>
2014-09-02 12:59:36 +0200

    Add a join method to HelperUtils
    Joins a String iterable into a string.

--- common-crawl/src/main/java/cc/topicexplorer/commoncrawl/HelperUtils.java
+++ common-crawl/src/main/java/cc/topicexplorer/commoncrawl/HelperUtils.java
@@ -4,16 +4,30 @@ import java.io.File;
 import org.apache.commons.io.FileUtils;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
-
-class HelperUtils {
+public class HelperUtils {
 
     // TODO: write test cases
     public static List<String> loadFileAsArray(String path) throws IOException {
             String contents = FileUtils.readFileToString(new File(path));
             String[] lines = contents.split("\n");
             return Arrays.asList(lines);
+    }
+
+    public static String join(Iterable<String> i, String separator) {
+        StringBuilder builder = new StringBuilder();
+        Iterator<String> iter = i.iterator();
+        if (iter.hasNext()) {
+            builder.append(iter.next());
+            while(iter.hasNext()) {
+                builder.append(separator);
+                builder.append(iter.next());
+            }
+        }
+
+        return builder.toString();
     }
 }
 

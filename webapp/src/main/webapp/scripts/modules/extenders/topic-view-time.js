ddd9cfb27557b994c4327217e41f44aa755c127f
Mattes Angelus <mattes.angelus@japanologie.uni-halle.de>
2014-09-09 10:14:54 +0200

    #134 decide by activated plugins which functions shall be provided

--- webapp/src/main/webapp/scripts/modules/extenders/topic-view-time.js
+++ webapp/src/main/webapp/scripts/modules/extenders/topic-view-time.js
@@ -1,8 +1,10 @@
 define(["knockout", "jquery"],
 function(ko, $) {
 	return function (instance) {
-		instance.loadTimeViewForTopic = function (topicId) { 
-			ko.postbox.publish('openNewTab', {moduleName:"time-chart-tab", tabHeading:"Time Chart " + topicId, data:{topicId: topicId}});	
-		};
+		if(globalData.PLUGINS.indexOf('time') > -1) {
+			instance.loadTimeViewForTopic = function (topicId) { 			
+				ko.postbox.publish('openNewTab', {moduleName:"time-chart-tab", tabHeading:"Time Chart " + topicId, data:{topicId: topicId}});	
+			};
+		}
 	};	
 });

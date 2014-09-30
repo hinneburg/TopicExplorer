d7cd97da796af695e3656cf06252396ae77e6d1d
Florian Lücke <florian.luecke2@gmail.com>
2014-09-30 13:31:52 +0200

    Fix URLFilter#filer(String)
    some urls were considered valid, because they contained parts of valid
    URLs

--- common-crawl/src/test/java/cc/topicexplorer/commoncrawl/test/URLFilterTest.java
+++ common-crawl/src/test/java/cc/topicexplorer/commoncrawl/test/URLFilterTest.java
@@ -1,5 +1,6 @@
 package cc.topicexplorer.commoncrawl.test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
@@ -11,19 +12,32 @@ import cc.topicexplorer.commoncrawl.HelperUtils;
 import cc.topicexplorer.commoncrawl.URLFilter;
 
 public class URLFilterTest {
-    private static final String VALID_URL_FILE_NAME  = "/blogproviders.txt";
-    private static final String VALID_URL_FILE_PATH  = URLFilterTest.class.getResource(VALID_URL_FILE_NAME).getPath();
-    private static final String SAMPLE_URL_FILE_NAME = "/SampleURLs.txt";
-    private static final String SAMPLE_URL_FILE_PATH = URLFilterTest.class.getResource(SAMPLE_URL_FILE_NAME).getPath();
+    private static final String BLOG_PROVIDER_FILE_NAME  = "/blogproviders.txt";
+    private static final String BLOG_PROVIDER_FILE_PATH  = URLFilterTest.class.getResource(BLOG_PROVIDER_FILE_NAME).getPath();
+    private static final String VALID_URL_FILE_NAME = "/SampleURLs.txt";
+    private static final String VALID_URL_FILE_PATH = URLFilterTest.class.getResource(VALID_URL_FILE_NAME).getPath();
+    private static final String INVALID_URL_FILE_NAME = "/InvalidULRs.txt";
+    private static final String INVALID_URL_FILE_PATH = URLFilterTest.class.getResource(INVALID_URL_FILE_NAME).getPath();
 
     @Test
-    public void testFilter() throws IOException {
-        List<String> validURLs = HelperUtils.loadFileAsArray(VALID_URL_FILE_PATH);
-        List<String> sampleURLs = HelperUtils.loadFileAsArray(SAMPLE_URL_FILE_PATH);
+    public void testFilter_validURLs() throws IOException {
+        List<String> validURLs = HelperUtils.loadFileAsArray(BLOG_PROVIDER_FILE_PATH);
+        List<String> sampleURLs = HelperUtils.loadFileAsArray(VALID_URL_FILE_PATH);
 
         URLFilter filter = new URLFilter(validURLs);
         for (String url : sampleURLs) {
             assertTrue(url + " should be valid", filter.filter(url));
+        }
+    }
+
+    @Test
+    public void testFilter_invalidURLs() throws IOException {
+        List<String> validURLs = HelperUtils.loadFileAsArray(BLOG_PROVIDER_FILE_PATH);
+        List<String> sampleURLs = HelperUtils.loadFileAsArray(INVALID_URL_FILE_PATH);
+
+        URLFilter filter = new URLFilter(validURLs);
+        for (String url : sampleURLs) {
+            assertFalse(url + " should be invalid", filter.filter(url));
         }
     }
 }

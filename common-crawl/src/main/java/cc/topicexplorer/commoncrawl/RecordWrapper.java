a1995fc78e06baeb757a9424ea40d0850f063489
Florian Lücke <florian.luecke2@gmail.com>
2014-09-30 11:00:24 +0200

    Improve getHTTPBody

--- common-crawl/src/main/java/cc/topicexplorer/commoncrawl/RecordWrapper.java
+++ common-crawl/src/main/java/cc/topicexplorer/commoncrawl/RecordWrapper.java
@@ -18,6 +18,7 @@ public class RecordWrapper {
     private static final int    HTTP_HEADER_START_LEN = HTTP_HEADER_START.length();
     private static final String HTTP_HEADER_END       = "\r\n\r\n";
     private String              content               = null;
+    private String              httpBody              = null;
     private ArchiveRecordHeader header                = null;
 
     public RecordWrapper(ArchiveRecord rec) {
@@ -67,11 +68,15 @@ public class RecordWrapper {
      * @return the requested body
      */
     public String getHTTPBody() {
+        if (httpBody != null) {
+            return httpBody;
+        }
+
         String content = this.getContent();
 
         if (content.substring(0, HTTP_HEADER_START_LEN).equals(HTTP_HEADER_START)) {
-            String body = content.substring(content.indexOf("\r\n\r\n") + 4);
-            return body;
+            httpBody = content.substring(content.indexOf("\r\n\r\n") + 4);
+            return httpBody;
         }
         return content;
     }

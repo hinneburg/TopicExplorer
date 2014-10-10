bb3159b3f8744719308dd450aade4ab5d57ecc63
Florian Lücke <florian.luecke2@gmail.com>
2014-10-10 15:48:06 +0200

    Improve usability of URLFilter

--- common-crawl/src/main/java/cc/topicexplorer/commoncrawl/URLFilter.java
+++ common-crawl/src/main/java/cc/topicexplorer/commoncrawl/URLFilter.java
@@ -1,5 +1,6 @@
 package cc.topicexplorer.commoncrawl;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -11,12 +12,32 @@ public class URLFilter {
 
     public URLFilter(List<String> urlList) {
         String pattern = "{http,https}://{"
-                         + HelperUtils.join(urlList, ",") + "}";
+                         + HelperUtils.join(prepareURLs(urlList), ",") + "}";
         this.urlPattern = GlobPattern.compile(pattern);
     }
 
     public boolean filter(String url) {
         Matcher m = urlPattern.matcher(url);
         return m.matches();
+    }
+
+    /**
+     * Replace leading * with [^/] to make sure urls only match at the beginning
+     * of the line
+     * 
+     * @param urls
+     *            a list of urls to prepare
+     * @return list of prepared URLs
+     */
+    protected List<String> prepareURLs(List<String> urls) {
+        List<String> preparedURLs = new ArrayList<String>();
+        for (String string : urls) {
+            if (string.startsWith("*")) {
+                preparedURLs.add(string.replaceFirst("\\*", "[^/]"));
+            } else {
+                preparedURLs.add(string);
+            }
+        }
+        return preparedURLs;
     }
 }

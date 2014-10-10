e54d9973ff5a64cccab584d95676f7ffbfa1f28d
Mattes Angelus <mattes.angelus@japanologie.uni-halle.de>
2014-10-10 09:07:19 +0200

    #149 showdoc action delivers parent topic ids for each keyword, frames are ordered by frame type

--- core-webinterface/src/main/java/cc/topicexplorer/actions/showdoc/ShowDoc.java
+++ core-webinterface/src/main/java/cc/topicexplorer/actions/showdoc/ShowDoc.java
@@ -71,6 +71,8 @@ public class ShowDoc {
 		JSONObject topic = new JSONObject();
 		JSONArray topics = new JSONArray();
 		JSONObject all = new JSONObject();
+		
+		JSONObject topicListOfParents = new JSONObject();
 
 		int docId = 0;
 		long start = System.currentTimeMillis();
@@ -85,6 +87,28 @@ public class ShowDoc {
 
 			Long time = System.currentTimeMillis() - start;
 			logger.info(" DocQueryTime: " + time + " ms");
+			
+			if (Arrays.asList(properties.get("plugins").toString().split(",")).contains("hierarchicaltopic")) {
+				ArrayList<Integer> parentList = new ArrayList<Integer>();
+				int leafTopicId = -1;
+				ResultSet hierarchicalTopicRS = database.executeQuery("SELECT t1.TOPIC_ID, t2.TOPIC_ID AS PARENT_ID FROM TOPIC t1, TOPIC t2 "
+						+ "WHERE t1.HIERARCHICAL_TOPIC$START = t1.HIERARCHICAL_TOPIC$END AND "
+						+ "t1.HIERARCHICAL_TOPIC$START >= t2.HIERARCHICAL_TOPIC$START AND "
+						+ "t1.HIERARCHICAL_TOPIC$END <= t2.HIERARCHICAL_TOPIC$END AND "
+						+ "t2.HIERARCHICAL_TOPIC$START < t2.HIERARCHICAL_TOPIC$END ORDER BY t1.TOPIC_ID, PARENT_ID");
+				while(hierarchicalTopicRS.next()) {
+					if(leafTopicId != hierarchicalTopicRS.getInt("TOPIC_ID")) {
+						if(parentList != null) {
+							topicListOfParents.put(leafTopicId, parentList);
+							parentList.clear();
+						}
+						leafTopicId = hierarchicalTopicRS.getInt("TOPIC_ID");
+					}
+					parentList.add(hierarchicalTopicRS.getInt("PARENT_ID"));
+				}
+				topicListOfParents.put(leafTopicId, parentList);
+			}
+			
 
 			ResultSet topicRS = database.executeQuery(topicMap.getSQLString());
 
@@ -92,27 +116,42 @@ public class ShowDoc {
 				for (int i = 0; i < topicColumnList.size(); i++) {
 					topic.put(topicColumnList.get(i), topicRS.getString(topicColumnList.get(i)));
 				}
+				if (Arrays.asList(properties.get("plugins").toString().split(",")).contains("hierarchicaltopic")) {
+					topic.put("HIERARCHICAL_TOPIC$PARENT_IDS", topicListOfParents.get(topicRS.getString("TOPIC_ID")));
+				}
 				topics.add(topic);
 			}
 
 			// hack: the following should happen in frame class(es):
 			if (Arrays.asList(properties.get("plugins").toString().split(",")).contains("frame")) {
 				JSONObject frame = new JSONObject();
+				JSONObject frameTypes = new JSONObject();
 				JSONArray frames = new JSONArray();
+				String actFrameType = "";
+				
 				ResultSet frameRS = database.executeQuery("SELECT * FROM FRAME$FRAMES WHERE DOCUMENT_ID=" + docId
-						+ " ORDER BY START_POSITION DESC");
+						+ " ORDER BY FRAME_TYPE, START_POSITION DESC");
 				ResultSetMetaData frameRSMD = frameRS.getMetaData();
 				String[] frameColumnNames = new String[frameRSMD.getColumnCount()];
+				
 				for (int i = 0; i < frameRSMD.getColumnCount(); i++) {
 					frameColumnNames[i] = frameRSMD.getColumnName(i + 1);
 				}
 				while (frameRS.next()) {
+					if(!actFrameType.equals(frameRS.getString("FRAME_TYPE"))) {
+						if(!frames.isEmpty()) {
+							frameTypes.put(actFrameType, frames);
+							frames.clear();
+						}
+						actFrameType = frameRS.getString("FRAME_TYPE");
+					}
 					for (String frameColumnName : frameColumnNames) {
 						frame.put(frameColumnName, frameRS.getString(frameColumnName));
 					}
 					frames.add(frame);
 				}
-				doc.put("FRAME_LIST", frames);
+				frameTypes.put(actFrameType, frames);
+				doc.put("FRAME_LIST", frameTypes);
 			}
 			// hack end
 			doc.put("WORD_LIST", topics);

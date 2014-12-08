64812c5b4bb1bc7c21c88b7026353291cf20258c
Mattes Angelus <mattes.angelus@japanologie.uni-halle.de>
2014-12-08 11:38:19 +0100

    #30 second try, permit maxdate < midnight

--- webapp/src/main/webapp/scripts/modules/extenders/document-browse-time.js
+++ webapp/src/main/webapp/scripts/modules/extenders/document-browse-time.js
@@ -2,9 +2,50 @@ define(["knockout", "jquery", "moment"],
 function(ko, $, moment) {
 	return function (instance) {
 		if(globalData.PLUGINS.indexOf('time') > -1) {
-			instance.activateDatePicker = function() {
-				$( ".datepicker" ).datepicker();
-			}
+			
+			if(typeof instance.browseData.timeData == 'undefined') {
+				instance.browseData.timeData = {};
+				$.getJSON("JsonServlet?Command=GetDateRange")
+				.success(function(receivedParsedJson) {
+					instance.browseData.timeData.minDate = receivedParsedJson.DateRange[0] * 1000;
+					instance.browseData.timeData.maxDate = receivedParsedJson.DateRange[1] * 1000;
+					$.datepicker.setDefaults({
+						minDate: new Date(instance.browseData.timeData.minDate),
+						maxDate: new Date(instance.browseData.timeData.maxDate)
+					});
+					instance.browseData.activateDatePicker()
+				});
+				
+				instance.browseData.activateDatePicker = function() {
+					if(instance.browseData[instance.active()].filter.firstDate().length == 0) {
+						$.datepicker.setDefaults({
+							minDate: new Date(instance.browseData.timeData.minDate)
+						});
+					} else {
+						$.datepicker.setDefaults({
+							minDate: new Date(instance.browseData[instance.active()].filter.firstDate())
+						});
+					}
+					
+					if(instance.browseData[instance.active()].filter.lastDate().length == 0) {
+						$.datepicker.setDefaults({
+							maxDate: new Date(instance.browseData.timeData.maxDate)
+						});
+					} else {
+						$.datepicker.setDefaults({
+							maxDate: new Date(instance.browseData[instance.active()].filter.lastDate())
+						});
+					}
+						
+					$(".datepicker").datepicker();
+					
+				}
+				
+				$(document).delegate(".datepicker", "focusin", function(){
+					   $(this).datepicker();
+					});
+				
+			} 
 			if(instance.browseData[instance.active()].sortingOptions.indexOf('Time') == -1) {
 				instance.browseData[instance.active()].activeFilter.firstDate = ko.observable("");
 				instance.browseData[instance.active()].activeFilter.lastDate = ko.observable("");
@@ -12,23 +53,15 @@ function(ko, $, moment) {
 				instance.browseData[instance.active()].filter.firstDate = ko.observable("");
 				instance.browseData[instance.active()].filter.lastDate = ko.observable("");
 				
-				instance.browseData[instance.active()].filter.firstDate.subscribe(instance.activateDatePicker);
-				instance.browseData[instance.active()].filter.lastDate.subscribe(instance.activateDatePicker);
+				instance.browseData[instance.active()].filter.firstDate.subscribe(instance.browseData.activateDatePicker);
+				instance.browseData[instance.active()].filter.lastDate.subscribe(instance.browseData.activateDatePicker);
 				
 				
 				instance.browseData[instance.active()].sortingOptions.push('Time');
 				
-				$.getJSON("JsonServlet?Command=GetDateRange")
-					.success(function(receivedParsedJson) {
-						$.datepicker.setDefaults({
-							minDate: new Date(receivedParsedJson.DateRange[0] * 1000),
-							maxDate: new Date(receivedParsedJson.DateRange[1] * 1000)
-						});
-						$( ".datepicker" ).datepicker();
-				});
 				
 			}
-			
+			instance.browseData.activateDatePicker();
 			
 			
 		}

bc49cbc02183a65d8fb64667c6556fde451f1bea
Mattes Angelus <mattes.angelus@japanologie.uni-halle.de>
2015-04-29 13:25:09 +0200

    #211 clean up properties: remove unneeded properties, new cmdb property for CRAWL_MANAGEMENT DB, new properties for user creation

--- core-common/src/test/java/cc/topicexplorer/commands/PropertiesCommandTest.java
+++ core-common/src/test/java/cc/topicexplorer/commands/PropertiesCommandTest.java
@@ -11,13 +11,14 @@ import org.junit.Test;
 
 import cc.commandmanager.core.Context;
 
+
 public class PropertiesCommandTest {
 
 	PropertiesCommand commandUnderTest = new PropertiesCommand();
 
 	@Test
 	public void testExecute() throws IOException {
-		byte[] targetPropertiesRaw = "plugin_hierarchicaltopic=true\nOrgTableId=id\nBitDepth=32\ndatabase.NumberOfRetries=3\nplugin_wordtype=true\nDocBrowserLimit=20\nmalletNumTopics=10\nOrgTableTxt=txt\ndatabase.DbUser=root\nplugins=text,hierarchicaltopic,colortopic,wordtype,fulltext\ndatabase.DbLocation=localhost:3306\ndatabase.DbPassword=TopicExplorer\nplugin_text=true\nFrontendViews=slider,topic,text\nRAM=1g\nFulltext_OrgTableFulltext=txt\nplugin_fulltext=true\nplugin_colortopic=true\ndatabase.DB=japantest\nInCSVFile=/home/user/japantest/db.csv\nOrgTableName=db"
+		byte[] targetPropertiesRaw = "plugin_hierarchicaltopic=true\n\ndatabase.NumberOfRetries=3\nplugin_wordtype=true\nDocBrowserLimit=20\nmalletNumTopics=10\ndatabase.DbUser=te_db_user\nplugins=text,hierarchicaltopic,colortopic,wordtype,fulltext\ndatabase.DbLocation=localhost:3306\ndatabase.DbPassword=te_db_password\nplugin_text=true\nplugin_fulltext=true\nplugin_colortopic=true\ndatabase.DB=te_db\nInCSVFile=/home/user/japantest/db.csv"
 				.getBytes();
 		InputStream propertiesStream = new ByteArrayInputStream(targetPropertiesRaw);
 		Properties targetProperties = new Properties();

69bfe7e2869949a2d6fc5d46685579020d7cfd23
Alexander Hinneburg <hinneburg@informatik.uni-halle.de>
2015-05-06 17:19:04 +0200

    #215: split createTable function
    into several parts (generate query and
    run query). Introduce functions for
    different implementations: simple, iterate
    documents, iterate pos.

--- plugin-mecab-initcorpus/src/main/java/cc/topicexplorer/plugin/mecab/initcorpus/command/AllTermsCreate.java
+++ plugin-mecab-initcorpus/src/main/java/cc/topicexplorer/plugin/mecab/initcorpus/command/AllTermsCreate.java
@@ -1,6 +1,8 @@
 package cc.topicexplorer.plugin.mecab.initcorpus.command;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 import org.apache.log4j.Logger;
@@ -11,20 +13,11 @@ import cc.topicexplorer.commands.TableCreateCommand;
 
 public class AllTermsCreate  extends TableCreateCommand {
 	private static final Logger logger = Logger.getLogger(AllTermsCreate.class);
+	private ArrayList<String> queries = new ArrayList<String>();
 
 	@Override
 	public void createTable() {
-		try {
-			this.database.executeUpdateQuery("CREATE TABLE `" + this.tableName + "` AS "
-					+ "SELECT DOCUMENT_TERM.TERM, COUNT(DISTINCT DOCUMENT_TERM.DOCUMENT_ID) AS COUNT, p2.POS "
-					+ "FROM DOCUMENT_TERM, POS_TYPE p1, POS_TYPE p2 "
-					+ "WHERE p1.POS=DOCUMENT_TERM.WORDTYPE_CLASS AND p1.LOW>=p2.LOW "
-					+ "AND p1.HIGH<=p2.HIGH "
-					+ "GROUP BY DOCUMENT_TERM.TERM, p2.POS"); 
-		} catch (SQLException e) {
-			logger.error("Table " + this.tableName + " could not be created.");
-			throw new RuntimeException(e);
-		}
+		runSqlFillQueriesSimple();
 	}
 
 	@Override
@@ -51,4 +44,47 @@ public class AllTermsCreate  extends TableCreateCommand {
 	public Set<String> getOptionalBeforeDependencies() {
 		return Sets.newHashSet();
 	}
+	private void generateFillQueriesSimple() {
+		queries.add(
+				"CREATE TABLE `" + this.tableName + "` AS "
+				+ "SELECT DOCUMENT_TERM.TERM, COUNT(DISTINCT DOCUMENT_TERM.DOCUMENT_ID) AS COUNT, p2.POS "
+				+ "FROM DOCUMENT_TERM, POS_TYPE p1, POS_TYPE p2 "
+				+ "WHERE p1.POS=DOCUMENT_TERM.WORDTYPE_CLASS AND p1.LOW>=p2.LOW "
+				+ "AND p1.HIGH<=p2.HIGH "
+				+ "GROUP BY DOCUMENT_TERM.TERM, p2.POS"
+				);
+	}
+	private void runSqlFillQueriesSimple() {
+		
+		try {
+			for (Integer i=0;i<queries.size();i++) {
+				logger.info("Start Query " + i.toString());
+				this.database.executeUpdateQuery(queries.get(i));
+				logger.info("Finished Query " + i.toString());
+			}
+			 
+		} catch (SQLException e) {
+			logger.error("Table " + this.tableName + " could not be created.");
+			throw new RuntimeException(e);
+		}
+
+	}
+
+	private void generateFillQueriesIterateDocuments() {
+		
+	}
+
+	private void runSqlFillQueriesIterateDocuments() {
+		
+	}
+
+	private void generateFillQueriesIteratePos() {
+		
+	}
+
+	private void runSqlFillQueriesIteratePos() {
+		
+	}
+
+
 }

5e314f73361d7864d787eca2e0a5ae5b3305b530
Alexander Hinneburg <hinneburg@informatik.uni-halle.de>
2015-05-19 11:54:39 +0200

    #223 included logger message about the recieved command.

--- webapp/src/main/java/cc/topicexplorer/web/JsonServlet.java
+++ webapp/src/main/java/cc/topicexplorer/web/JsonServlet.java
@@ -16,6 +16,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -28,9 +29,13 @@ import cc.topicexplorer.database.Database;
 public class JsonServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
+	private static final Logger logger = Logger.getLogger(JsonServlet.class);
+	
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String command = request.getParameter("Command");
+		
+		logger.info("Recieved request with command: " + command);
 
 		response.setCharacterEncoding("UTF8");
 

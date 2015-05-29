fe6d9a529aac44e0c347642cdd330853fcc25946
Alexander Hinneburg <hinneburg@informatik.uni-halle.de>
2015-05-29 11:38:10 +0200

    #228 added simple test for empty properties

--- plugin-frame-preprocessing/src/test/java/cc/topicexplorer/plugin/frame/preprocessing/tables/frames/FrameFillTest.java
+++ plugin-frame-preprocessing/src/test/java/cc/topicexplorer/plugin/frame/preprocessing/tables/frames/FrameFillTest.java
@@ -1,14 +1,24 @@
 package cc.topicexplorer.plugin.frame.preprocessing.tables.frames;
 
-import org.junit.Ignore;
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.junit.Test;
 
 public class FrameFillTest {
 
-	@Ignore
 	@Test
-	public void testFillTable() {
+	public void testFillTable() throws IOException {
 		// TODO cover test cases of public FrameFill methods
-	}
 
+		Properties p = new Properties();
+
+		p.load(new ByteArrayInputStream("test=".getBytes()));
+
+		String[] startWordTypes = p.getProperty("test").split(",");
+		assertEquals(true, startWordTypes[0].isEmpty());
+	}
 }

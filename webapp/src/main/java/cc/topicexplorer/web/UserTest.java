b877bd561fd58c13f7ba648662a5a9398c5b315f
Chris Rothe <adbad@inf-ts.informatik.uni-halle.de>
2015-09-15 08:56:33 +0200

    #163 migrate CommandManager 1.0
    updated executeCommands(...) routine to CommandManager 1.0 signature

--- webapp/src/main/java/cc/topicexplorer/web/UserTest.java
+++ webapp/src/main/java/cc/topicexplorer/web/UserTest.java
@@ -2,6 +2,7 @@ package cc.topicexplorer.web;
 
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -14,19 +15,21 @@ public class UserTest extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
+	@SuppressWarnings("unchecked")
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Context context = WebChainManagement.getContext();
 		context.bind("SERVLET_WRITER", response.getWriter());
-		WebChainManagement.executeCommands(WebChainManagement.getOrderedCommands(new HashSet<String>()), context);
+		WebChainManagement.executeCommands((List<String>) new HashSet<String>(), context);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
-			IOException {
+	IOException {
 
 	}
 

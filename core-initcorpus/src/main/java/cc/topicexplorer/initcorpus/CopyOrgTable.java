306a56b68ee25682ec66ee5392a90b993b927b9f
Alexander Hinneburg <hinneburg@informatik.uni-halle.de>
2017-05-11 17:39:04 +0200

    #250 use filter for corpus creation

--- core-initcorpus/src/main/java/cc/topicexplorer/initcorpus/CopyOrgTable.java
+++ core-initcorpus/src/main/java/cc/topicexplorer/initcorpus/CopyOrgTable.java
@@ -48,61 +48,163 @@ public class CopyOrgTable implements Command {
 		String dbName = properties.getProperty("database.DB");
 		String searchStringId = properties.getProperty("database.SearchStringId");
 
+		// Construct the filter for the new corpus
+		String corpusFilter = "";
+		String topicExplorer_DefinitionIsPresent = "";
 		try {
-			Statement copyStmt = crawlManagerConnection.createStatement(java.sql.ResultSet.TYPE_FORWARD_ONLY,
-					java.sql.ResultSet.CONCUR_READ_ONLY);
-			copyStmt.setFetchSize(Integer.MIN_VALUE);
 
-			Statement tmpStmt;
+			Statement checkStmt = crawlManagerConnection.createStatement();
+			ResultSet resultSetOfCheck = checkStmt.executeQuery("select count(*) as IS_THERE "
+					+ "from  INFORMATION_SCHEMA.Tables "
+					+ "where TABLE_NAME='TOPIC_EXPLORER_DEFINITION' and TABLE_SCHEMA=DATABASE();");
+			if (resultSetOfCheck.next()) {
+				topicExplorer_DefinitionIsPresent = resultSetOfCheck.getString("IS_THERE");
+				logger.info("Test if TOPIC_EXPLORER_DEFINITION table is present, gave result: "
+						+ topicExplorer_DefinitionIsPresent);
 
-			ResultSet tableRs = copyStmt.executeQuery("SELECT TABLE_NAME FROM CRAWL WHERE SEARCH_STRING_ID="
-					+ searchStringId);
-			int maxId = 0;
-			List<String> tableNames = new ArrayList<String>();
-			while (tableRs.next()) {
-				tableNames.add(tableRs.getString("TABLE_NAME"));
 			}
-			copyStmt.close();
-			for (String tableName : tableNames) {
-				tmpStmt = crawlManagerConnection.createStatement();
-				if (maxId == 0) {
-					tmpStmt.executeUpdate("CREATE TABLE " + dbName
-							+ ".orgTable_meta AS (SELECT *, MD5(URL) AS URL_MD5 FROM " + tableName + ")");
-					tmpStmt.executeUpdate("CREATE TABLE " + dbName + ".orgTable_text AS (SELECT * FROM " + tableName
-							+ "_TEXT)");
+		} catch (SQLException e) {
+			logger.error("Can not check if TOPIC_EXPLORER_DEFINITION table is present.");
+			throw new RuntimeException(e);
+		}
 
+		if ("1".equals(topicExplorer_DefinitionIsPresent)) {
+			try {
+
+				Statement filterStmt = crawlManagerConnection.createStatement();
+				ResultSet resultSetOfFilter = filterStmt.executeQuery("select " + "  FILTER_START_DATETIME,"
+						+ "  FILTER_END_DATETIME," + "  FILTER_TEXT_SQL " + "from " + "  TOPIC_EXPLORER_DEFINITION "
+						+ "where" + "  TE_IDENTIFIER = '" + dbName + "';");
+				// using this filter in a query assumes that org_table_meta and
+				// org_table_text have been joined
+				logger.info("Construct corpus filter with statement:" + "select " + "  FILTER_START_DATETIME,"
+						+ "  FILTER_END_DATETIME," + "  FILTER_TEXT_SQL " + "from " + "  TOPIC_EXPLORER_DEFINITION "
+						+ "where" + "  TE_IDENTIFIER = '" + dbName + "';");
+				if (resultSetOfFilter.next()) {
+					corpusFilter = " " + " ( DOCUMENT_DATE BETWEEN " + "CAST('"
+							+ resultSetOfFilter.getString("FILTER_START_DATETIME") + "' AS DATE) AND " + "CAST('"
+							+ resultSetOfFilter.getString("FILTER_END_DATETIME") + "' AS DATE) " + ") AND ( "
+							+ resultSetOfFilter.getString("FILTER_TEXT_SQL") + " ) ";
+
+				}
+				logger.info("Corpus Filter is: " + corpusFilter);
+			} catch (SQLException e) {
+				logger.error("Filter Condition could not be created.");
+				throw new RuntimeException(e);
+			}
+		}
+
+		if ("1".equals(topicExplorer_DefinitionIsPresent)) {
+			// when the new table Topic_EXPLORER_DEFINITION is there
+			// than we can use the filter
+			try {
+				logger.info("Use corpus filter to : create orgTables");
+
+				Statement copyStmt = crawlManagerConnection.createStatement(java.sql.ResultSet.TYPE_FORWARD_ONLY,
+						java.sql.ResultSet.CONCUR_READ_ONLY);
+				copyStmt.setFetchSize(Integer.MIN_VALUE);
+
+				ResultSet tableRs = copyStmt.executeQuery("SELECT TABLE_NAME FROM CRAWL WHERE SEARCH_STRING_ID="
+						+ searchStringId);
+				// there should be only one table per SEARCH_STRING_ID
+				if (tableRs.next()) {
+					String tableName = tableRs.getString("TABLE_NAME");
+					copyStmt.close();
+
+					Statement tmpStmt = crawlManagerConnection.createStatement();
+					//@formatter:off
+					tmpStmt.executeUpdate(
+							"CREATE TABLE " + dbName + ".orgTable_meta AS ("
+									+ "SELECT " + tableName + ".*, MD5(URL) AS URL_MD5 "
+									+ "FROM " + tableName + " join " + tableName + "_TEXT using (DOCUMENT_ID) "
+									+ "WHERE " + corpusFilter 
+									+ " )"
+							);
+					tmpStmt.executeUpdate(
+							"CREATE TABLE " + dbName + ".orgTable_text AS ("
+									+ "SELECT " + tableName	+ "_TEXT.* "
+									+ "FROM " + tableName + " join " + tableName + "_TEXT using (DOCUMENT_ID) "
+									+ "WHERE " + corpusFilter 
+									+ " )"
+							);
+					//@formatter:on
 					tmpStmt.executeUpdate("create index url_idx on " + dbName + ".orgTable_meta(URL_MD5,DOCUMENT_DATE)");
 					tmpStmt.executeUpdate("create index id_idx on " + dbName + ".orgTable_meta(DOCUMENT_ID)");
-
 					tmpStmt.executeUpdate("create index id_idx on " + dbName + ".orgTable_text(DOCUMENT_ID)");
+					tmpStmt.close();
 				} else {
-					// create temp tables
-					tmpStmt.executeUpdate("CREATE TABLE " + dbName + ".tempTable_meta AS (SELECT * FROM " + tableName
-							+ ")");
-					tmpStmt.executeUpdate("CREATE TABLE " + dbName + ".tempTable_text AS (SELECT * FROM " + tableName
-							+ "_TEXT)");
-					// update ids
-					tmpStmt.executeUpdate("UPDATE " + dbName + ".tempTable_meta SET DOCUMENT_ID=DOCUMENT_ID+" + maxId);
-					tmpStmt.executeUpdate("UPDATE " + dbName + ".tempTable_text SET DOCUMENT_ID=DOCUMENT_ID+" + maxId);
-					// copy
-					tmpStmt.executeUpdate("INSERT INTO " + dbName
-							+ ".orgTable_meta SELECT *, MD5(URL) AS URL_MD5 FROM " + dbName + ".tempTable_meta");
-					tmpStmt.executeUpdate("INSERT INTO " + dbName + ".orgTable_text SELECT * FROM " + dbName
-							+ ".tempTable_text");
-					// delete
-					tmpStmt.executeUpdate("DROP TABLE " + dbName + ".tempTable_meta");
-					tmpStmt.executeUpdate("DROP TABLE " + dbName + ".tempTable_text");
+					logger.error("No table name found for SEARCH_STRING_ID" + searchStringId);
+					throw new RuntimeException();
 				}
-				ResultSet maxIdRs = tmpStmt.executeQuery("SELECT MAX(DOCUMENT_ID) FROM " + dbName + ".orgTable_meta");
-				if (maxIdRs.next()) {
-					maxId = maxIdRs.getInt(1);
-				}
-				tmpStmt.close();
-			}
 
-		} catch (SQLException e) {
-			logger.error("OrgTables could not be created.");
-			throw new RuntimeException(e);
+			} catch (SQLException e) {
+				logger.error("OrgTables could not be created with Filter Condition.");
+				throw new RuntimeException(e);
+			}
+		} else {
+			// when the new table Topic_EXPLORER_DEFINITION is not there
+			// than we fall back to the old code
+
+			try {
+				Statement copyStmt = crawlManagerConnection.createStatement(java.sql.ResultSet.TYPE_FORWARD_ONLY,
+						java.sql.ResultSet.CONCUR_READ_ONLY);
+				copyStmt.setFetchSize(Integer.MIN_VALUE);
+
+				Statement tmpStmt;
+
+				ResultSet tableRs = copyStmt.executeQuery("SELECT TABLE_NAME FROM CRAWL WHERE SEARCH_STRING_ID="
+						+ searchStringId);
+				int maxId = 0;
+				List<String> tableNames = new ArrayList<String>();
+				while (tableRs.next()) {
+					tableNames.add(tableRs.getString("TABLE_NAME"));
+				}
+				copyStmt.close();
+				for (String tableName : tableNames) {
+					tmpStmt = crawlManagerConnection.createStatement();
+					if (maxId == 0) {
+						tmpStmt.executeUpdate("CREATE TABLE " + dbName
+								+ ".orgTable_meta AS (SELECT *, MD5(URL) AS URL_MD5 FROM " + tableName + ")");
+						tmpStmt.executeUpdate("CREATE TABLE " + dbName + ".orgTable_text AS (SELECT * FROM "
+								+ tableName + "_TEXT)");
+
+						tmpStmt.executeUpdate("create index url_idx on " + dbName
+								+ ".orgTable_meta(URL_MD5,DOCUMENT_DATE)");
+						tmpStmt.executeUpdate("create index id_idx on " + dbName + ".orgTable_meta(DOCUMENT_ID)");
+
+						tmpStmt.executeUpdate("create index id_idx on " + dbName + ".orgTable_text(DOCUMENT_ID)");
+					} else {
+						// create temp tables
+						tmpStmt.executeUpdate("CREATE TABLE " + dbName + ".tempTable_meta AS (SELECT * FROM "
+								+ tableName + ")");
+						tmpStmt.executeUpdate("CREATE TABLE " + dbName + ".tempTable_text AS (SELECT * FROM "
+								+ tableName + "_TEXT)");
+						// update ids
+						tmpStmt.executeUpdate("UPDATE " + dbName + ".tempTable_meta SET DOCUMENT_ID=DOCUMENT_ID+"
+								+ maxId);
+						tmpStmt.executeUpdate("UPDATE " + dbName + ".tempTable_text SET DOCUMENT_ID=DOCUMENT_ID+"
+								+ maxId);
+						// copy
+						tmpStmt.executeUpdate("INSERT INTO " + dbName
+								+ ".orgTable_meta SELECT *, MD5(URL) AS URL_MD5 FROM " + dbName + ".tempTable_meta");
+						tmpStmt.executeUpdate("INSERT INTO " + dbName + ".orgTable_text SELECT * FROM " + dbName
+								+ ".tempTable_text");
+						// delete
+						tmpStmt.executeUpdate("DROP TABLE " + dbName + ".tempTable_meta");
+						tmpStmt.executeUpdate("DROP TABLE " + dbName + ".tempTable_text");
+					}
+					ResultSet maxIdRs = tmpStmt.executeQuery("SELECT MAX(DOCUMENT_ID) FROM " + dbName
+							+ ".orgTable_meta");
+					if (maxIdRs.next()) {
+						maxId = maxIdRs.getInt(1);
+					}
+					tmpStmt.close();
+				}
+
+			} catch (SQLException e) {
+				logger.error("OrgTables could not be created.");
+				throw new RuntimeException(e);
+			}
 		}
 		return ResultState.success();
 
--- core-initcorpus/src/main/java/cc/topicexplorer/initcorpus/RemoveDuplicates.java
+++ core-initcorpus/src/main/java/cc/topicexplorer/initcorpus/RemoveDuplicates.java
@@ -1,6 +1,7 @@
 package cc.topicexplorer.initcorpus;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
@@ -13,7 +14,6 @@ import cc.commandmanager.core.Context;
 import cc.commandmanager.core.ResultState;
 
 import com.google.common.collect.Sets;
-
 
 public class RemoveDuplicates implements Command {
 
@@ -29,28 +29,49 @@ public class RemoveDuplicates implements Command {
 
 		this.tableName = dbName + ".DUPLICATES";
 
-
+		String topicExplorer_DefinitionIsPresent = "";
 
 		try {
-			Statement stmt = crawlManagerConnection.createStatement();
-
-			stmt.executeUpdate("create table " + this.tableName + " as "
-					+ "select URL_MD5, min(DOCUMENT_DATE) mindate, min(DOCUMENT_ID) minid from " + dbName + ".orgTable_meta "
-					+ "group by URL_MD5 having count(*) > 1");
-
-			stmt.executeUpdate("create index url_idx on " + this.tableName + "(URL_MD5,mindate,minid)");
-
-			stmt.executeUpdate("delete " + dbName + ".orgTable_meta, " + dbName + ".orgTable_text from "
-					+ dbName + ".orgTable_meta, " + dbName + ".orgTable_text, " + this.tableName
-					+ " where orgTable_meta.DOCUMENT_ID=orgTable_text.DOCUMENT_ID and orgTable_meta.URL_MD5=" + this.tableName + ".URL_MD5 and "
-					+ "(orgTable_meta.DOCUMENT_DATE>" + this.tableName + ".mindate or (orgTable_meta.DOCUMENT_DATE=" + this.tableName + ".mindate "
-					+ "and orgTable_meta.DOCUMENT_ID>" + this.tableName + ".minid))");
-
-			stmt.executeUpdate("drop table " + this.tableName);
-
+			Statement checkStmt = crawlManagerConnection.createStatement();
+			ResultSet resultSetOfCheck = checkStmt.executeQuery("select count(*) as IS_THERE "
+					+ "from  INFORMATION_SCHEMA.Tables "
+					+ "where TABLE_NAME='TOPIC_EXPLORER_DEFINITION' and TABLE_SCHEMA=DATABASE();");
+			if (resultSetOfCheck.next()) {
+				topicExplorer_DefinitionIsPresent = resultSetOfCheck.getString("IS_THERE");
+			}
 		} catch (SQLException e) {
-			logger.error("Table " + this.tableName + " could not be filled properly.");
-			return ResultState.failure("Table " + this.tableName + " could not be filled properly.", e);
+			logger.error("Can not check for table TOPIC_EXPLORER_DEFINITION in mangagement database.");
+			return ResultState.failure(
+					"Can not check for table TOPIC_EXPLORER_DEFINITION in mangagement database. Table "
+							+ this.tableName + " could not be filled properly.", e);
+		}
+
+		if ("0".equals(topicExplorer_DefinitionIsPresent)) {
+			// Old Crawl Management Database, this may contain duplicates in the
+			// documents
+			// that need to be handled.
+			try {
+				Statement stmt = crawlManagerConnection.createStatement();
+
+				stmt.executeUpdate("create table " + this.tableName + " as "
+						+ "select URL_MD5, min(DOCUMENT_DATE) mindate, min(DOCUMENT_ID) minid from " + dbName
+						+ ".orgTable_meta " + "group by URL_MD5 having count(*) > 1");
+
+				stmt.executeUpdate("create index url_idx on " + this.tableName + "(URL_MD5,mindate,minid)");
+
+				stmt.executeUpdate("delete " + dbName + ".orgTable_meta, " + dbName + ".orgTable_text from " + dbName
+						+ ".orgTable_meta, " + dbName + ".orgTable_text, " + this.tableName
+						+ " where orgTable_meta.DOCUMENT_ID=orgTable_text.DOCUMENT_ID and orgTable_meta.URL_MD5="
+						+ this.tableName + ".URL_MD5 and " + "(orgTable_meta.DOCUMENT_DATE>" + this.tableName
+						+ ".mindate or (orgTable_meta.DOCUMENT_DATE=" + this.tableName + ".mindate "
+						+ "and orgTable_meta.DOCUMENT_ID>" + this.tableName + ".minid))");
+
+				stmt.executeUpdate("drop table " + this.tableName);
+
+			} catch (SQLException e) {
+				logger.error("Table " + this.tableName + " could not be filled properly.");
+				return ResultState.failure("Table " + this.tableName + " could not be filled properly.", e);
+			}
 		}
 		return ResultState.success();
 
@@ -75,7 +96,5 @@ public class RemoveDuplicates implements Command {
 	public Set<String> getOptionalBeforeDependencies() {
 		return Sets.newHashSet();
 	}
-
-
 
 }

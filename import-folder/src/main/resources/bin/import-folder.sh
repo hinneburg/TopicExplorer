b8ea4a74fb3cf70fddff177b4bf1e792d04a1a29
Alexander Hinneburg <hinneburg@informatik.uni-halle.de>
2018-12-09 11:54:05 +0100

    fix missing dot in caller script #263

--- import-folder/src/main/resources/bin/import-folder.sh
+++ import-folder/src/main/resources/bin/import-folder.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-retcode=`java -Dfile.encoding=UTF-8 -classpath "etc:resources:lib/*:." cc.topicexplorer.dataimportImportFolder --folder ${1}`
+retcode=`java -Dfile.encoding=UTF-8 -classpath "etc:resources:lib/*:." cc.topicexplorer.dataimport.ImportFolder --folder ${1}`
